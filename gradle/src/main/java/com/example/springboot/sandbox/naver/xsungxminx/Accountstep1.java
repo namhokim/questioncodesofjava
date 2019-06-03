105c9edf35a45cfc0cf0812ecab6229325132da3
namo <nhkimnamo@gmail.com>
2019-06-03 14:50:24 +0900

    문제 구현내용 : 자바의정석은행에서 웹을 이용하여 계좌를 관리할 수 있도록 시스템을 구축하려고 한다. Accountstep1클래스를 이용하여 실행 결과가 아래와 같이 나오도록 필드에 값을 저장하고, 적절한 메소드를 호출해 보도록 한다.
    실행결과 :
    계좌 123-456789(예금주 : 홍길동)
    잔액 : 10000원
    20000 원 입금합니다.
    잔액 : 30000원
    45000원 출금합니다.
    잔액: -15000원
    
    https://cafe.naver.com/javachobostudy/161509

---
+++ gradle/src/main/java/com/example/springboot/sandbox/naver/xsungxminx/Accountstep1.java
@@ -0,0 +1,46 @@
+package com.example.springboot.sandbox.naver.xsungxminx;
+
+public class Accountstep1 {
+    private String accountNumber;
+    private String accountHolder;
+    private int balance;
+
+    public Accountstep1(String accountNumber, String accountHolder, int initialBalance) {
+        this.accountNumber = accountNumber;
+        this.accountHolder = accountHolder;
+        this.balance = initialBalance;
+
+        System.out.printf("계좌 %s(예금주 : %s)%n", accountNumber, accountHolder);
+        System.out.printf("잔액 : %d원%n", balance);
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public String getAccountHolder() {
+        return accountHolder;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void deposit(int amount) {
+        balance += amount;
+        System.out.printf("%d 원 입금합니다.%n", amount);
+        System.out.printf("잔액 : %d원%n", balance);
+    }
+
+    public void withdraw(int amount) {
+        balance -= amount;
+        System.out.printf("%d 원 인출합니다.%n", amount);
+        System.out.printf("잔액 : %d원%n", balance);
+    }
+
+    public static void main(String[] args) {
+        Accountstep1 accountstep = new Accountstep1("123-456789", "홍길동", 10_000);
+        accountstep.deposit(20_000);
+        accountstep.withdraw(45_000);
+    }
+}
