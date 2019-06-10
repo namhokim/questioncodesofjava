91aa1e5dd01ddf451e2cd8c240d7d0616fe8dcc5
namo <nhkimnamo@gmail.com>
2019-06-10 19:36:21 +0900

    항상 Scanner를 이용해서 출력값을 nextInt()등으로 받아서 썼는데, 이런건 처음 봐서요. 이 경우 Buffredread의 값이 return값으로 나오잖아요,
    그러면 Main에서
    그럼 Main 클래스에서 이 출력값을 쓰고 싶으면 어떻게 해야할까요??
    
    제가 하고싶은건, console 사용자에게 메뉴를 보여주고 1-3에서 선택해라 물은 후,
    사용자가 1-3의 숫자 하나를 선택하는 경우 그 선택에 따라서 다른 method를 호출하는걸 하고싶은데
    입력받는 부분에서 막히네요
    
    https://cafe.naver.com/javachobostudy/161650

---
+++ gradle/src/main/java/com/example/springboot/sandbox/naver/ssimba323/ConsoleHelper.java
@@ -0,0 +1,37 @@
+package com.example.springboot.sandbox.naver.ssimba323;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+
+public class ConsoleHelper {
+
+    private BufferedReader in;
+    private PrintStream out;
+
+    private ConsoleHelper(BufferedReader reader, PrintStream out) {
+        this.in = reader;
+        this.out = out;
+    }
+
+    private static ConsoleHelper build() {
+        BufferedReader reader = new BufferedReader((new InputStreamReader(System.in)));
+        return new ConsoleHelper(reader, System.out);
+    }
+
+    public static void main(String[] args) throws IOException {
+        String[] menu = {"Coffee", "Tea", "Water"};
+        ConsoleHelper helper = ConsoleHelper.build();
+        helper.out.println("Menu");
+        helper.out.println("1. Coffee");
+        helper.out.println("2. Tea");
+        helper.out.println("3. Water");
+        helper.out.print("? ");
+
+        String order = helper.in.readLine();
+        final int menuNumber = Integer.parseInt(order);
+        helper.out.printf("You choose %s", menu[menuNumber - 1]);
+
+    }
+}
