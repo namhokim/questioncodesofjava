8b589740fd57712154ecb7f2e17859d71fe8879e
namo <nhkimnamo@gmail.com>
2019-06-20 17:02:42 +0900

    일반 Innerclass(non-static)에 static field를 선언하며 에러가 나는 원인이 무엇인가요? Static은 객체가 만들어지기도 전에 이미 메모리상에 존재하는데 왜 에러가 나는지 모르겠네요
    https://cafe.naver.com/javachobostudy/161767

---
+++ gradle/src/main/java/com/example/springboot/sandbox/naver/goharrm/innerclass/InnerClassInStaticProperty.java
@@ -0,0 +1,8 @@
+package com.example.springboot.sandbox.naver.goharrm.innerclass;
+
+public class InnerClassInStaticProperty {
+    public class InnerClass {
+        private static final String X = "x";
+        // private static String app; // error
+    }
+}
