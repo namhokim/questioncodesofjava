700cf24d39dd8c3e83d8d89efdf3415613500016
namhokim <nhkimnamo@gmail.com>
2019-07-29 22:51:13 +0900

    방법3. String.split 을 이용

--- gradle/src/main/java/com/example/springboot/sandbox/naver/o6o4_/Main.java
+++ gradle/src/main/java/com/example/springboot/sandbox/naver/o6o4_/Main.java
@@ -1,17 +1,13 @@
 package com.example.springboot.sandbox.naver.o6o4_;
 
-import java.util.StringTokenizer;
-
 public class Main {
     public static void main(String[] args) {
         String str = "1,2,3,4,5,6,7";
 
-        StringTokenizer st = new StringTokenizer(str, ",");
-
-        int countTokens = st.countTokens();
-        System.out.println("토큰 수 : " + countTokens);
-        while (st.hasMoreTokens()) {
-            System.out.println(st.nextToken());
+        String[] tokens = str.split(",");
+        System.out.println("토큰 수 : " + tokens.length);
+        for (String token : tokens) {
+            System.out.println(token);
         }
     }
 }
