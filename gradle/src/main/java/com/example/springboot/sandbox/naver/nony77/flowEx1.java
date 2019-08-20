2cb06626946d20b4636f160d97574af980c1660b
namhokim <nhkimnamo@gmail.com>
2019-08-20 23:52:52 +0900

    Create flowEx1.java

---
+++ gradle/src/main/java/com/example/springboot/sandbox/naver/nony77/flowEx1.java
@@ -0,0 +1,29 @@
+package com.example.springboot.sandbox.naver.nony77;
+
+public class flowEx1 {
+    static void main(String[] args) {
+        int x = 0;
+        System.out.printf("x=%d 일때, 참인것은%n", x);
+        if(x==0)
+            System.out.println("x==0");
+
+        if(x!=0)  System.out.println("x!=0");
+
+        if(!(x==0))System.out.println(!(x==0));
+
+        if(!(x!=0))System.out.println(!(x!=0));
+
+        x = 1;
+
+        System.out.printf("x=%d 일때, 참인것은%n", x);
+
+        if(x==0) System.out.println("x==0");
+
+        if(x!=0)  System.out.println("x!=0");
+
+        if(!(x==0))System.out.println(!(x==0));
+
+        if(!(x!=0))
+            System.out.println(!(x!=0));
+    }
+}
