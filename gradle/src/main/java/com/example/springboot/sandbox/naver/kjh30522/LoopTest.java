317bf02740367defbebe1c74c8e70abc3f863894
namhokim <nhkimnamo@gmail.com>
2019-10-05 16:00:28 +0900

    코드 리뷰 1. Replace with <> 2. Lambda can be replaced with method reference
    및 설명 코드 추가

--- gradle/src/main/java/com/example/springboot/sandbox/naver/kjh30522/LoopTest.java
+++ gradle/src/main/java/com/example/springboot/sandbox/naver/kjh30522/LoopTest.java
@@ -2,23 +2,37 @@ package com.example.springboot.sandbox.naver.kjh30522;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Consumer;
 
 public class LoopTest {
-    public static void main(String[] args) {
-        List<String> list = new ArrayList<String>();
 
-        for (int i=0; i<10; i++) {
-            list.add(""+i);
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+
+        for (int i = 0; i < 10; i++) {
+            list.add(String.valueOf(i));
+            list.add(Integer.toString(i));
         }
 
         // 1
-        list.forEach(str-> {
-            System.out.println(str);
-        });
+        list.forEach(System.out::println);
+        list.forEach(new PrintWithLinefeed());
 
         // 2
-        for(String str : list) {
+        for (String str : list) {
             System.out.println(str);
         }
+//        Iterator<String> iterator = list.iterator();
+//        while (iterator.hasNext()) {
+//            System.out.println(iterator.next());
+//        }
+
+    }
+}
+
+class PrintWithLinefeed implements Consumer<String> {
+    @Override
+    public void accept(String s) {
+        System.out.println(s);
     }
 }
