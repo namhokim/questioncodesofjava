677d1ad73cfd3d50c08866d9e4325d2b596a36eb
namhokim <nhkimnamo@gmail.com>
2019-10-13 17:17:33 +0900

    Refactoring

--- gradle/src/main/java/com/example/springboot/sandbox/naver/lkt0520/A023.java
+++ gradle/src/main/java/com/example/springboot/sandbox/naver/lkt0520/A023.java
@@ -10,21 +10,26 @@ public class A023 {
         System.out.println("숫자 2번을 입력하세요");
         int num2 = scanner.nextInt();
         System.out.println("연산자를 입력하세요");
-        char ch = scanner.next().charAt(0);
-        // 처리
-        double sum = 0;
-        switch (ch) {
-            case '+': sum = num1 + num2; { break; }
-            case '-': sum = num1 - num2; { break; }
-            case '*': sum = num1 * num2; { break; }
-            case '/': sum = num1 / (double)num2; { break; }
+        char op = scanner.next().charAt(0); // operator (연산자)라는 의미로 이름을 부여했습니ㅣ다.
+        double result = 0; // sum (합)이라는 의미는 연산자에 따라 달라지므로 의미가 부적합해보여 결과(result)로 이름을 바꾸었습니다.
+        switch (op) {
+            case '+':
+                result = num1 + num2;
+                break;  // { break;} 형태 제거1
+            case '-':
+                result = num1 - num2;
+                break;  // { break;} 형태 제거2
+            case '*':
+                result = num1 * num2;
+                break;  // { break;} 형태 제거3
+            case '/':
+                result = num1 / (double) num2;
+                break;  // { break;} 형태 제거4
+            default:    // 잘못된 연산자에 예외 추가
+                throw new IllegalArgumentException(op + "는 잘못된 연산자입니다.");
         }
 
         // 출력
-        if (ch == '/') {
-        System.out.println(num1 + ch + num2 + "=" + (double)sum);
-        }
-        else
-        System.out.println(num1 + ch + num2 + "=" + sum);
+        System.out.printf("%d%c%d=%f", num1, op, num2, result); // 포맷팅된 (formatted) 출력으로 변경
     }
 }
