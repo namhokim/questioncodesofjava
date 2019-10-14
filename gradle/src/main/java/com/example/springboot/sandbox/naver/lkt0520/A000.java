98272f66720640d8d26618fe07e6a2aeff60344c
namhokim <nhkimnamo@gmail.com>
2019-10-14 23:10:05 +0900

    int 타입 flag를 boolean으로 변경함 값을 가져오는 부분을 메서드로 추출

--- gradle/src/main/java/com/example/springboot/sandbox/naver/lkt0520/A000.java
+++ gradle/src/main/java/com/example/springboot/sandbox/naver/lkt0520/A000.java
@@ -2,12 +2,30 @@ package com.example.springboot.sandbox.naver.lkt0520;
 
 public class A000 {
     public static void main(String[] args) {
-        int flag = 1; int i = 1;int num = 0, sum = 0;
-        for( sum = 0, flag = 1, i = 1;  ; i++,flag=-flag) {
-            num = i*flag;
-            sum = sum + num;
-            if(sum == 100) { break; }
+        int sum = 0;
+        boolean isPositive = true;
+        for (int i = 1; ; i++) {
+            int currentValue = getValueBy(i, isPositive);
+            sum = sum + currentValue;
+            // System.out.println(i + ": " +sum);
+            if (sum == 100) {
+                System.out.println(currentValue + "/" + sum);
+                break;
+            }
+            isPositive = !isPositive;
         }
-        System.out.println(num + "/" + sum);
+    }
+
+    /**
+     * 양/음 플래그에 의해 값을 반환한다.
+     * @param value 값
+     * @param isPositive 양수 여부
+     * @return isPositive 가 반영된 값
+     */
+    private static int getValueBy(int value, boolean isPositive) {
+        if (isPositive) {
+            return value;
+        }
+        return -value;
     }
 }
