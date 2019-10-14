cae70c9533a850c46ab45633e16ea92caa8001b9
namhokim <nhkimnamo@gmail.com>
2019-10-14 23:25:43 +0900

    integer overflow에 대한 ArithmeticException 예외가 발생하도록 변경

--- gradle/src/main/java/com/example/springboot/sandbox/naver/lkt0520/A000.java
+++ gradle/src/main/java/com/example/springboot/sandbox/naver/lkt0520/A000.java
@@ -2,17 +2,17 @@ package com.example.springboot.sandbox.naver.lkt0520;
 
 public class A000 {
     public static void main(String[] args) {
-        int targetValue = 100;
+        int targetValue = Integer.MAX_VALUE;
         int index = findNumberOfTimes(targetValue);
         System.out.println(index + "/" + targetValue);
     }
 
     private static int findNumberOfTimes(int value) {
         if (value > 0) {
-            return (value * 2) - 1;
+            return Math.multiplyExact(value, 2) - 1;
         }
         if (value < 0) {
-            return -value * 2;
+            return Math.multiplyExact(-value, 2);
         }
         throw new IllegalArgumentException("Cannot reach 0");
     }
