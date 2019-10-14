86eebd66c7323173aea563096c2efc32290b0716
namhokim <nhkimnamo@gmail.com>
2019-10-14 23:22:11 +0900

    O(n) 알고리즘을 O(1)로 변경함.

--- gradle/src/main/java/com/example/springboot/sandbox/naver/lkt0520/A000.java
+++ gradle/src/main/java/com/example/springboot/sandbox/naver/lkt0520/A000.java
@@ -2,30 +2,18 @@ package com.example.springboot.sandbox.naver.lkt0520;
 
 public class A000 {
     public static void main(String[] args) {
-        int sum = 0;
-        boolean isPositive = true;
-        for (int i = 1; ; i++) {
-            int currentValue = getValueBy(i, isPositive);
-            sum = sum + currentValue;
-            // System.out.println(i + ": " +sum);
-            if (sum == 100) {
-                System.out.println(currentValue + "/" + sum);
-                break;
-            }
-            isPositive = !isPositive;
-        }
+        int targetValue = 100;
+        int index = findNumberOfTimes(targetValue);
+        System.out.println(index + "/" + targetValue);
     }
 
-    /**
-     * 양/음 플래그에 의해 값을 반환한다.
-     * @param value 값
-     * @param isPositive 양수 여부
-     * @return isPositive 가 반영된 값
-     */
-    private static int getValueBy(int value, boolean isPositive) {
-        if (isPositive) {
-            return value;
+    private static int findNumberOfTimes(int value) {
+        if (value > 0) {
+            return (value * 2) - 1;
         }
-        return -value;
+        if (value < 0) {
+            return -value * 2;
+        }
+        throw new IllegalArgumentException("Cannot reach 0");
     }
 }
