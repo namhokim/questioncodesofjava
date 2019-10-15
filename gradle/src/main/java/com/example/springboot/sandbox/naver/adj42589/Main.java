31929d9889e505bdb928ff278e4e40771e2323e2
namhokim <nhkimnamo@gmail.com>
2019-10-16 07:59:46 +0900

    클래스 선언하는 연습문제인데 Member에 자꾸 빨간줄로 밑줄이 그어지내요 ㅠㅠㅠㅜ 해결방법좀 가르켜 주세요
    https://cafe.naver.com/javachobostudy/163902

---
+++ gradle/src/main/java/com/example/springboot/sandbox/naver/adj42589/Main.java
@@ -0,0 +1,34 @@
+package com.example.springboot.sandbox.naver.adj42589;
+
+public class Main {
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        public class Member{
+
+
+            String name;
+            String id;
+            String password;
+            int age;
+
+            Member(String id, String password){
+                this(id,password);
+
+            }
+            Member(String name,String password,String id){
+                this(name,password,id);
+
+            }
+
+
+            Member(String name,String password,String id,int age){
+                this.name=name;
+                this.password=password;
+                this.id=id;
+                this.age=age;
+            }
+
+
+        }
+    }
+}
