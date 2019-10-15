143bb66a6b1c013d48203a2730543462c1c3a112
namhokim <nhkimnamo@gmail.com>
2019-10-16 08:06:14 +0900

    error: class Member is public, should be declared in a file named Member.java Recursive constructor invocation

--- gradle/src/main/java/com/example/springboot/sandbox/naver/adj42589/Main.java
+++ gradle/src/main/java/com/example/springboot/sandbox/naver/adj42589/Main.java
@@ -1,34 +1,26 @@
 package com.example.springboot.sandbox.naver.adj42589;
 
-public class Main {
-    public static void main(String[] args) {
-        // TODO Auto-generated method stub
-        public class Member{
+public class Member{
+    String name;
+    String id;
+    String password;
+    int age;
 
-
-            String name;
-            String id;
-            String password;
-            int age;
-
-            Member(String id, String password){
-                this(id,password);
-
-            }
-            Member(String name,String password,String id){
-                this(name,password,id);
-
-            }
-
-
-            Member(String name,String password,String id,int age){
-                this.name=name;
-                this.password=password;
-                this.id=id;
-                this.age=age;
-            }
-
-
-        }
+    Member(String id, String password){
+        this(id,password);
     }
+    Member(String name,String password,String id){
+        this(name,password,id);
+
+    }
+
+
+    Member(String name,String password,String id,int age){
+        this.name=name;
+        this.password=password;
+        this.id=id;
+        this.age=age;
+    }
+
+
 }
