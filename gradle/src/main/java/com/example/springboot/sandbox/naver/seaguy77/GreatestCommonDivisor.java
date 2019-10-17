6c1694b46d662658544d8a4abb2002601d6a25ed
namo <nhkimnamo@gmail.com>
2019-10-17 14:57:54 +0900

    total  이 32비트로 인식되어 강제적으로 64비트로 인식되게 하려고 합니다.  로직에러는 없는데 32비트 초과시 처리가 안됨
     https://cafe.naver.com/javachobostudy/163930

---
+++ gradle/src/main/java/com/example/springboot/sandbox/naver/seaguy77/GreatestCommonDivisor.java
@@ -0,0 +1,18 @@
+package com.example.springboot.sandbox.naver.seaguy77;
+
+public class GreatestCommonDivisor {
+    public static void main(String[] args) {
+        int n = Integer.MAX_VALUE / 2;
+        int cnt = 5;
+        long total = 1;
+        for(int i = 1; i<=n; i++)
+            total *= 2;   // => 이부분이 안먹음
+        long G = gcd(total, cnt);
+        System.out.println(G);
+    }
+
+    private static long gcd(long total, int cnt) {
+        System.out.println(total);
+        return total;
+    }
+}
