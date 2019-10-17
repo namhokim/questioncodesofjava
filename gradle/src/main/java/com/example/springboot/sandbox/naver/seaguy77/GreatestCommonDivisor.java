8f5c976ade0cc12eca7cb19756748da4cf9d8569
namo <nhkimnamo@gmail.com>
2019-10-17 15:14:40 +0900

    64번부터는 0이되는 것으로 확인 long으로 표현할 수 있는 9223372036854775807의 값을 초과하는 9223372036854775808 가 되기 때문임. 오버플로우가 나지 않고 0으로 되는 이유는 1 << 64 처럼 동작하기 때문으로 추측됨.
    실제 바이트코드는
        BIPUSH 64
        LSHL
    가 아닌
        LDC 2
        LMUL
    로 만들어지는 것으로 확인

--- gradle/src/main/java/com/example/springboot/sandbox/naver/seaguy77/GreatestCommonDivisor.java
+++ gradle/src/main/java/com/example/springboot/sandbox/naver/seaguy77/GreatestCommonDivisor.java
@@ -5,8 +5,13 @@ public class GreatestCommonDivisor {
         int n = Integer.MAX_VALUE / 2;
         int cnt = 5;
         long total = 1;
-        for(int i = 1; i<=n; i++)
+        for (int i = 1; i <= n; i++) {
             total *= 2;   // => 이부분이 안먹음
+            if (total == 0) {
+                System.out.println(i);  // 64
+                break;
+            }
+        }
         long G = gcd(total, cnt);
         System.out.println(G);
     }
