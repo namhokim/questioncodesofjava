7e94a94cefa6b6a02f07b42baf98f1fc4bee02a6
namo <nhkimnamo@gmail.com>
2019-10-18 22:54:46 +0900

    ???

--- gradle/src/main/java/com/example/springboot/sandbox/naver/party7458/NthGreateNumberFinder.java
+++ gradle/src/main/java/com/example/springboot/sandbox/naver/party7458/NthGreateNumberFinder.java
@@ -1,27 +1,14 @@
 package com.example.springboot.sandbox.naver.party7458;
 
-import java.util.stream.IntStream;
-
 public class NthGreateNumberFinder {
-    private static int N = 10;
-
-    public static void main(String[] args) {
-        int[] a = IntStream.range(1, 1 + 5).toArray();
-        int[] b = IntStream.range(4, 4 + 5).toArray();
-        int nTh = 3;
-        System.out.println(findO_N(a, b, nTh));
-    }
-
     /**
      * O(n) 알고리즘
      */
-    static int findO_N(int[] a, int[] b, int nTh) {
-        if (nTh < 1) {
-            return -1;
-        }
-
-        int ai = a.length -1;
-        int bi = b.length -1;
+    static int findO_N(int[] a, int[] b) {
+        assert (a.length == b.length);
+        int nTh = a.length;
+        int ai = a.length - 1;
+        int bi = b.length - 1;
         int remainDepth = nTh;
 
         while (ai >= 0 && bi >= 0) {
@@ -38,36 +25,38 @@ public class NthGreateNumberFinder {
             }
             --remainDepth;
         }
-
-//        if (nTh > a.length + b.length) {
-//            return -1;
-//        }
-//
-//        if (a[a.length-1] <= b[b.length-1]) {
-//            return find2(a, b, nTh);
-//        }
-//        return find2(b, a, nTh);
         return -1;
     }
 
     /**
-     * 비교
-     * @param a 보다 작은 배열
-     * @param b 최대값이 포함한 배열
-     * @param nTh n번째 큰수의 n
-     * @return n번째 큰수
+     * log(N)
      */
-    static int find2(int[] a, int[] b, int nTh) {
-        int nHtOfB = b.length - nTh;
-        if (nHtOfB < 0) {
-            int nHtOfA = a.length - (nTh - b.length);
-            if (nHtOfA < 0) {
-                return -1;
-            }
-            return a[nHtOfA];
-        }
-        int v = b[nHtOfB];
+    static int findO_LogN(int[] A, int[] B) {
+        int m = A.length, n = B.length;
+        int l = (m + n + 1) / 2;
+        int r = (m + n + 2) / 2;
+        return (getkth(A, 0, B, 0, l) + getkth(A, 0, B, 0, r)) / 2;
+    }
 
-        return v;
+    static int getkth(int[] A, int aStart, int[] B, int bStart, int k) {
+        if (aStart > A.length - 1) {
+            return B[bStart + k - 1];
+        }
+        if (bStart > B.length - 1) {
+            return A[aStart + k - 1];
+        }
+        if (k == 1) {
+            return Math.max(A[aStart], B[bStart]);
+        }
+
+        int aMid = Integer.MAX_VALUE, bMid = Integer.MAX_VALUE;
+        if (aStart + k / 2 - 1 < A.length) aMid = A[aStart + k / 2 - 1];
+        if (bStart + k / 2 - 1 < B.length) bMid = B[bStart + k / 2 - 1];
+
+        if (aMid < bMid) {
+            return getkth(A, aStart + k / 2, B, bStart, k - k / 2); // Check: aRight + bLeft
+        } else {
+            return getkth(A, aStart, B, bStart + k / 2, k - k / 2); // Check: bRight + aLeft
+        }
     }
 }
--- gradle/src/test/groovy/com/example/springboot/sandbox/naver/party7458/NthGreateNumberFinderTest.groovy
+++ gradle/src/test/groovy/com/example/springboot/sandbox/naver/party7458/NthGreateNumberFinderTest.groovy
@@ -7,40 +7,43 @@ class NthGreateNumberFinderTest extends Specification {
     @Unroll
     def "배열의 수보다 횟수가 작을 경우 -1을 반환한다."() {
         when:
-        def result = NthGreateNumberFinder.findO_N(a, b, nTh)
+        def result = NthGreateNumberFinder.findO_LogN(a, b)
 
         then:
-        result == -1
+        result == expectedResult
 
         where:
-        a            | b            | nTh
-        [1] as int[] | [2] as int[] | 3
-        [1] as int[] | [1] as int[] | 3
-        [1] as int[] | [1] as int[] | 4
+        a            | b            || expectedResult
+        [1] as int[] | [2] as int[] || 2
+        [1] as int[] | [1] as int[] || 1
+        [1] as int[] | [0] as int[] || 1
     }
 
+    @Unroll
     def "균형있게 섞여있는 경우"() {
         when:
-        def result = NthGreateNumberFinder.findO_N(a, b, nTh)
+        def result = NthGreateNumberFinder.findO_LogN(a, b)
 
         then:
         result == expectedResult
 
         where:
-        a                  | b                  | nTh || expectedResult
-        [1, 3, 5] as int[] | [2, 4, 7] as int[] | 3   || 4
+        a                   | b                    || expectedResult
+        [1, 3, 5] as int[]  | [2, 4, 7] as int[]   || 4
+        [1, 4, 10] as int[] | [3, 11, 12] as int[] || 10
     }
 
+    @Unroll
     def "구역별로 나뉘어 있는 경우"() {
         when:
-        def result = NthGreateNumberFinder.findO_N(a, b, nTh)
+        def result = NthGreateNumberFinder.findO_N(a, b)
 
         then:
         result == expectedResult
 
         where:
-        a                  | b                  | nTh || expectedResult
-        [1, 2, 3] as int[] | [5, 6, 7] as int[] | 3   || 5
-        [5, 6, 7] as int[] | [1, 2, 3] as int[] | 3   || 5
+        a                  | b                  || expectedResult
+        [1, 2, 3] as int[] | [5, 6, 7] as int[] || 5
+        [5, 6, 7] as int[] | [1, 2, 3] as int[] || 5
     }
 }
