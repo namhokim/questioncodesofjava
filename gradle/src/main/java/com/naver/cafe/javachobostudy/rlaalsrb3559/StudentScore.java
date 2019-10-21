5bcb4041710b125d918df50a20ad8eb98465a02d
namo <nhkimnamo@gmail.com>
2019-10-21 11:59:22 +0900

    1. JavaDoc from multiline comment 2. indentation 3. introduce instance variations (totalScore, averageScore) 4. introduce defence check for not initialized values (totalScore, averageScore)

--- gradle/src/main/java/com/naver/cafe/javachobostudy/rlaalsrb3559/StudentScore.java
+++ gradle/src/main/java/com/naver/cafe/javachobostudy/rlaalsrb3559/StudentScore.java
@@ -1,39 +1,61 @@
 package com.naver.cafe.javachobostudy.rlaalsrb3559;
+
 import java.util.Scanner;
 
-/*사용자로부터 5명의 성적을 입력 받아 총점과 평균을 구하는 클래스 StudentScore 를 작성하시오.
-메소드 getScore() 는 인원수 만큼 학생의 성적을 입력 받아 score 배열에 저장
-메소드 totalScore 는 배열 score[] 변수에 있는 값을 이용하여 총점을 계산하고 totalScore 변수에 저장
-메소드 calcAverage() 는 총점과 인원수를 이용하여 평균을 계산하고 averageScore 변수에 저장
-메소드 printResult() 는 인원수, 총점 과 평균을 출력
-테스트를 위해 클래스 Test 를 생성하고 클래스 StudentScore 를 호출하여 각 메소드를 수행한다.
-*/
-public class StudentScore {
-    final int STUDENT=5;
-    int total=0;
-    Scanner scan= new Scanner(System.in);
-    int scores[]=new int[STUDENT];
+/**
+ * 사용자로부터 5명의 성적을 입력 받아 총점과 평균을 구하는 클래스 StudentScore 를 작성하시오.
+ * 메소드 getScore() 는 인원수 만큼 학생의 성적을 입력 받아 score 배열에 저장
+ * 메소드 totalScore 는 배열 score[] 변수에 있는 값을 이용하여 총점을 계산하고 totalScore 변수에 저장
+ * 메소드 calcAverage() 는 총점과 인원수를 이용하여 평균을 계산하고 averageScore 변수에 저장
+ * 메소드 printResult() 는 인원수, 총점 과 평균을 출력
+ * 테스트를 위해 클래스 Test 를 생성하고 클래스 StudentScore 를 호출하여 각 메소드를 수행한다.
+ */
+class StudentScore {
+    private static final int STUDENT = 5;
+    private static final int NOT_INITIALIZED = -1;
 
-    public void getScore() {
-        for(int i = 0;i<STUDENT;i++) {
+    private int[] scores = new int[STUDENT];
+    private int totalScore = NOT_INITIALIZED;
+    private int averageScore = NOT_INITIALIZED;
+
+    void getScore() {
+        Scanner scan = new Scanner(System.in);
+        for (int i = 0; i < STUDENT; i++) {
             System.out.println("성적을 입력하세요: ");
-            scores[i]=scan.nextInt();
+            scores[i] = scan.nextInt();
         }
     }
 
-    public void totalScore() {
-        for(int i=0;i<STUDENT;i++) {
-            total+=scores[i];
-            int totalScore = total;
+    void totalScore() {
+        int total = 0;
+        for (int i = 0; i < STUDENT; i++) {
+            total += scores[i];
         }
+        this.totalScore = total;
     }
 
-    public void calcAverage() {
-        int averageScore=total/STUDENT;
+    void calcAverage() {
+        if (this.totalScore == NOT_INITIALIZED) {
+            totalScore();
+        }
+        this.averageScore = this.totalScore / STUDENT;
     }
 
-    public void printResult() {
-        System.out.println("총점은: "+total);
-        System.out.println("평균은: ");
+    void printResult() {
+        if (this.averageScore == NOT_INITIALIZED) {
+            calcAverage();
+        }
+        System.out.println("총점은: " + this.totalScore);
+        System.out.println("평균은: " + this.averageScore);
+    }
+}
+
+class Test {
+    public static void main(String[] args) {
+        StudentScore studentScore = new StudentScore();
+        studentScore.getScore();
+        studentScore.totalScore();
+        studentScore.calcAverage();
+        studentScore.printResult();
     }
 }
