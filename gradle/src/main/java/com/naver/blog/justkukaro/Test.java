6ced71df60cf0455bda0fbaaad49e70142f1ab62
namo <nhkimnamo@gmail.com>
2019-10-21 16:25:22 +0900

    설명에는 E가 모두 Integer로 치환된다고 써있으나 컴파일된 코드에는 Object로 바뀌는 것을 확인할 수 있었음. 따라서 거짓!

--- gradle/src/main/java/com/naver/blog/justkukaro/Test.java
+++ gradle/src/main/java/com/naver/blog/justkukaro/Test.java
@@ -2,25 +2,49 @@ package com.naver.blog.justkukaro;
 
 public class Test {
     public static void main(String[] args) {
-        Point<Integer> p = new Point<Integer>(10, 20);
+        Point<Integer> p = new Point<>(10, 20);
         //Point p = new Point(10, 20);
-        p.Print();
+        p.print();
     }
 }
 
 class Point<E> {
     private E x;
     private E y;
-    Point (E x, E y) {
+
+    Point(E x, E y) {
         this.x = x;
         this.y = y;
     }
+
     void swap() {
         E tmp = x;
-        x=y;
-        y=tmp;
+        x = y;
+        y = tmp;
     }
-    void Print() {
+
+    void print() {
+        System.out.println(x);
+        System.out.println(y);
+    }
+}
+
+class PointAssume {
+    private Integer x;
+    private Integer y;
+
+    PointAssume(Integer x, Integer y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    void swap() {
+        Integer tmp = x;
+        x = y;
+        y = tmp;
+    }
+
+    void print() {
         System.out.println(x);
         System.out.println(y);
     }
