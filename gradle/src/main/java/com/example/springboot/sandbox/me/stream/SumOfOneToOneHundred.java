809abc844b26ef8db701186b312f304d2715abe9
namhokim <nhkimnamo@gmail.com>
2019-10-27 11:53:29 +0900

    BinaryOperator에 대해 알려주세요 어떤 원리에 의한건가요?
    https://cafe.naver.com/javachobostudy/164108

---
+++ gradle/src/main/java/com/example/springboot/sandbox/me/stream/SumOfOneToOneHundred.java
@@ -0,0 +1,11 @@
+package com.example.springboot.sandbox.me.stream;
+
+public class SumOfOneToOneHundred {
+    public static void main(String[] args) {
+        int sum = 0;
+        for (int i = 1; i <= 100; i++) {
+            sum += i;
+        }
+        System.out.println(sum);
+    }
+}
--- gradle/src/main/java/com/naver/cafe/javachobostudy/effect132/ReduceWithBinaryOp.java
+++ gradle/src/main/java/com/naver/cafe/javachobostudy/effect132/ReduceWithBinaryOp.java
@@ -1,22 +1,25 @@
 package com.naver.cafe.javachobostudy.effect132;
 
-        import java.util.Arrays;
-        import java.util.List;
-        import java.util.function.BinaryOperator;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.BinaryOperator;
 
 public class ReduceWithBinaryOp {
     public static void main(String[] args) {
         List<String> ls = Arrays.asList("Adam", "BYW", "Complex", "Robot");
 
-        BinaryOperator<String> lc = (s1, s2) -> {
-            if (s1.length() > s2.length()) {
-                return s1;
-            } else {
-                return s2;
-            }
-        };
+        String shortestString = ls.stream().reduce("", new SurviveLongString());
+        System.out.println(shortestString);
+    }
+}
 
-        String str = ls.parallelStream().reduce("", lc);
-        System.out.println(str);
+class SurviveLongString implements BinaryOperator<String> {
+    @Override
+    public String apply(String s1, String s2) {
+        if (s1.length() > s2.length()) {
+            return s1;
+        } else {
+            return s2;
+        }
     }
 }
