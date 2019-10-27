35b799da40fc35918dba7f697b3306dd8f120019
namhokim <nhkimnamo@gmail.com>
2019-10-27 18:31:19 +0900

    logic to lambda

--- gradle/src/main/java/com/naver/cafe/javachobostudy/effect132/ReduceWithBinaryOp.java
+++ gradle/src/main/java/com/naver/cafe/javachobostudy/effect132/ReduceWithBinaryOp.java
@@ -2,19 +2,23 @@ package com.naver.cafe.javachobostudy.effect132;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.BinaryOperator;
 
 public class ReduceWithBinaryOp {
+    private static BinaryOperator<String> lc = (s1, s2) -> {
+        if (s1.length() > s2.length()) {
+            return s1;
+        } else {
+            return s2;
+        }
+    };
+
     public static void main(String[] args) {
         List<String> ls = Arrays.asList("Adam", "BYW", "Complex", "Robot");
 
-
         String str = "";
         for (String elem : ls) {
-            if (str.length() > elem.length()) {
-                str = str;
-            } else {
-                str = elem;
-            }
+            str = lc.apply(str, elem);
         }
 
         System.out.println(str);
