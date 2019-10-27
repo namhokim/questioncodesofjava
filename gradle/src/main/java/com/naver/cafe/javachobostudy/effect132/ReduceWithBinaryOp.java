4a3c697ab69961eca20983a0a06038b6c3dbc38f
namhokim <nhkimnamo@gmail.com>
2019-10-27 12:23:59 +0900

    replace to join from collect
    https://docs.oracle.com/javase/8/docs/api/java/util/stream/package-summary.html

--- gradle/src/main/java/com/naver/cafe/javachobostudy/effect132/ReduceWithBinaryOp.java
+++ gradle/src/main/java/com/naver/cafe/javachobostudy/effect132/ReduceWithBinaryOp.java
@@ -7,7 +7,7 @@ public class ReduceWithBinaryOp {
     public static void main(String[] args) {
         List<String> ls = Arrays.asList("Adam", "BYW", "Complex", "Robot");
 
-        String concatenated = ls.stream().reduce("", String::concat);
+        String concatenated = String.join("", ls);
         System.out.println(concatenated);
     }
 }
