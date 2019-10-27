a9c017f8ff5ad9b2d11c3051529f4264e5a3c2d8
namhokim <nhkimnamo@gmail.com>
2019-10-27 15:51:33 +0900

    add sum function

--- gradle/src/main/java/com/example/springboot/sandbox/me/stream/SumOfOneToOneHundred.java
+++ gradle/src/main/java/com/example/springboot/sandbox/me/stream/SumOfOneToOneHundred.java
@@ -4,7 +4,12 @@ import java.util.stream.IntStream;
 
 public class SumOfOneToOneHundred {
     public static void main(String[] args) {
-        int sum = IntStream.rangeClosed(1, 100).reduce(0, Integer::sum);
+        int sum = IntStream.rangeClosed(1, 201).reduce(0, Integer::sum);
         System.out.println(sum);
+        System.out.println(sum(1, 201));
+    }
+
+    private static int sum(int startInclusive, int endInclusive) {
+        return (startInclusive + endInclusive) * (endInclusive - startInclusive + 1) / 2;
     }
 }
