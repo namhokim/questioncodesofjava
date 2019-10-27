f07e8d4999fdeb340d9e463f93d74eae9c84718b
namhokim <nhkimnamo@gmail.com>
2019-10-27 11:55:57 +0900

    sum to IntStream

--- gradle/src/main/java/com/example/springboot/sandbox/me/stream/SumOfOneToOneHundred.java
+++ gradle/src/main/java/com/example/springboot/sandbox/me/stream/SumOfOneToOneHundred.java
@@ -1,11 +1,10 @@
 package com.example.springboot.sandbox.me.stream;
 
+import java.util.stream.IntStream;
+
 public class SumOfOneToOneHundred {
     public static void main(String[] args) {
-        int sum = 0;
-        for (int i = 1; i <= 100; i++) {
-            sum += i;
-        }
+        int sum = IntStream.rangeClosed(1, 100).reduce(0, (x, y) -> x + y);
         System.out.println(sum);
     }
 }
