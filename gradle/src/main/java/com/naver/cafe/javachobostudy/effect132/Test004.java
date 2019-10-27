135f1283b4d788dfc2662a1659c78b8c7ea3e8bc
namhokim <nhkimnamo@gmail.com>
2019-10-27 18:43:26 +0900

    lambda to reduce

--- gradle/src/main/java/com/naver/cafe/javachobostudy/effect132/Test004.java
+++ gradle/src/main/java/com/naver/cafe/javachobostudy/effect132/Test004.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.function.BinaryOperator;
 
-public class ReduceWithBinaryOp {
+public class Test004 {
     private static BinaryOperator<String> lc = (s1, s2) -> {
         if (s1.length() > s2.length()) {
             return s1;
@@ -16,10 +16,7 @@ public class ReduceWithBinaryOp {
     public static void main(String[] args) {
         List<String> ls = Arrays.asList("Adam", "BYW", "Complex", "Robot");
 
-        String str = "";
-        for (String elem : ls) {
-            str = lc.apply(str, elem);
-        }
+        String str = ls.stream().reduce("", lc);
 
         System.out.println(str);
     }
