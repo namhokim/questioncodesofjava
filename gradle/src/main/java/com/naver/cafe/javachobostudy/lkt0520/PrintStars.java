0567c91918b16d44ddba69625a814e37926524cb
namhokim <nhkimnamo@gmail.com>
2019-10-28 22:46:26 +0900

    - 질문 제목 : disp(int a, char b) a로 입력받은 값만큼 별로 변환하려고 하는데, 리턴값을 어떻게 줘야할지 모르겠네요..
    https://cafe.naver.com/javachobostudy/164139

---
+++ gradle/src/main/java/com/naver/cafe/javachobostudy/lkt0520/PrintStars.java
@@ -0,0 +1,8 @@
+package com.naver.cafe.javachobostudy.lkt0520;
+
+public class PrintStars {
+    public static int disp(int a, char b) { for(int i=0; i <a; i++) {  } return b;}
+    public static void main(String[] args) {
+        System.out.println(disp(7, '*'));
+    }
+}
