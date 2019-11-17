f57a95d640f7809030a551ff6097caee2bf4e62c
namhokim <nhkimnamo@gmail.com>
2019-11-17 17:53:54 +0900

    CrudRepositor가 인터페이스인데 CrudRepository를 상속 받아서 메소드를 오버라이딩 하지않고 메소드를 사용할 수 있는 이유를 알고싶습니다.
    https://cafe.naver.com/javachobostudy/164509

--- gradle/src/main/java/com/naver/cafe/javachobostudy/sa_nan/JavacCompile.java
+++ gradle/src/main/java/com/naver/cafe/javachobostudy/sa_nan/JavacCompile.java
@@ -1,10 +1,10 @@
 package com.naver.cafe.javachobostudy.sa_nan;
 
-import com.sun.tools.javac.Main;
+//import com.sun.tools.javac.Main;
 
 public class JavacCompile {
     public static void main(String[] args) throws Exception {
-        String[] arguments = new String[]{"/Volumes/RamDisk/Exe5.java"};
-        Main.main(arguments);
+        //String[] arguments = new String[]{"/Volumes/RamDisk/Exe5.java"};
+        //Main.main(arguments);
     }
 }
