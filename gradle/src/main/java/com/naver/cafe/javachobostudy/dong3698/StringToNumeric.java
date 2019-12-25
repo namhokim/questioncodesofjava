9a73f5b6753beebf299ed5599a4d82dc56cf3481
namhokim <nhkimnamo@gmail.com>
2019-12-25 20:16:03 +0900

    1. 현재 문자열을 숫자로 반환하기를 공부하고 있습니다. 예제를 보게 되면 int i = new Integer("100").intValue(); 이렇게 있으면은 new 연산자를 통해서 메모리에 Integer객체에 생성자 100을 넣는 모습이라 생각됩니다. 그리고 바로 그 객체의 메소드 intValue();를 실행하는 모습으로 보이는데 이게 맞는 것인지.
    맞다고 가정하고 int i2 = Integer.parseInt("100"); 이 문장을 보게 되면
    객체를 전혀 생성하지 않고(new 사용X) 곧바로 Integer를 실행하는 모습을 볼 수 있는데
    어떻게 이게 가능한 것인지 궁금합니다. 클래스 메소드일까요? 내부 동작좀 알려주시면 감사하겠습니다!
    
    ref. https://cafe.naver.com/javachobostudy/165272

---
+++ gradle/src/main/java/com/naver/cafe/javachobostudy/dong3698/StringToNumeric.java
@@ -0,0 +1,8 @@
+package com.naver.cafe.javachobostudy.dong3698;
+
+public class StringToNumeric {
+    public static void main(String[] args) {
+        int i = new Integer("100").intValue();
+        int i2 = Integer.parseInt("100");
+    }
+}
