c6aa135c13d9aee87925a031e8c516d49b132953
namo <nhkimnamo@gmail.com>
2020-02-07 11:42:37 +0900

    Companion class 인 Arrays 의 equals 메서드를 사용

--- gradle/src/main/java/com/naver/cafe/javachobostudy/rlawodns11111/ArrayCompare.java
+++ gradle/src/main/java/com/naver/cafe/javachobostudy/rlawodns11111/ArrayCompare.java
@@ -1,19 +1,14 @@
 package com.naver.cafe.javachobostudy.rlawodns11111;
 
+import java.util.Arrays;
+
 public class ArrayCompare {
     public static void main(String[] args) {
         int[] Arr1 = {1, 2, 3};
         int[] Arr2 = {1, 2, 3};
 
-        for (int i = 0; i < Arr1.length; i++) {
-            for (int j = 0; j < Arr2.length; j++) {
-                if (Arr1[i] == Arr2[j]) {
-                    System.out.println("같아요");
-                } else {
-                    System.out.println("달라요");
-                }
-            }
-        }
+        final boolean isEquals = Arrays.equals(Arr1, Arr2);
+        System.out.println(isEquals);
     }
 
 }
