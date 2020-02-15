5f5ca879bf74b7f9f31f1e57e2f5028d9246984e
namhokim <nhkimnamo@gmail.com>
2020-02-15 16:16:46 +0900

    - Beautiful Soup 제거 - json 추가 - 샘플 데이터 출력

--- gradle/src/main/java/com/naver/cafe/javachobostudy/disraeli12/stock.py
+++ gradle/src/main/java/com/naver/cafe/javachobostudy/disraeli12/stock.py
@@ -1,9 +1,11 @@
 import urllib.request
-import bs4
+import json
 
 url = "https://www.tipranks.com/api/stocks/getChartPageData/?ticker=PM&benchmark=1&period=3&break=1581737752401"
-html = urllib.request.urlopen(url)
+request = urllib.request.urlopen(url)
+data = request.read()
+encoding = request.info().get_content_charset('utf-8')
+json_data = json.loads(data.decode(encoding))
 
-bsobj = bs4.BeautifulSoup(html, "html.parser")
-
-print(bsobj)
+print(json_data["earnings"][0]["date"])         # date
+print(json_data["earnings"][0]["isConfirmed"])  # isConfirmed
