00b42c26b555fb84f32a39596902c3992b9a320d
namhokim <nhkimnamo@gmail.com>
2020-02-16 18:34:01 +0900

    기본적으로 변수에 값을 대입할때 ㅁㅁ형 변수 에 ㅁㅁ형 리터럴을 대입 하는 것으로 알고 있습니다. 다시 말하자면 형식을 일치시킨다는 말입니다.
    형식이 일치 하지 않을때여도 저장범위가 좁은 타입의 값을 저장범위가 넓은 타입에 저장하는 것은 가능하다고 알고 있습니다.
    
    char 형에 대한 질문입니다.
    char word1 = 'a';
    
    이게 char형 기본 입력 방식인것은 쉽게 알겠습니다.
    char형 변수에 char형 리터럴을 대입하는것입니다.
    
    하지만 아래의 입력방식을 이해하지 못하겠습니다.
    
    char word2 = 97;
    
    char형 변수에 int형 리터럴을 대입하는 것입니다.
    
    형식이 일치 하지 않을때 이고 저장범위가 좁은 타입의 값을 저장범위가 넓은 타입에 저장하는 것도 아닙니다.
    어떻게 이게 가능한가요??
    부탁드립니다.
    
    ref. https://cafe.naver.com/javachobostudy/166122

---
+++ gradle/src/main/java/com/naver/cafe/javachobostudy/muk2008/IntToChar.java
@@ -0,0 +1,14 @@
+package com.naver.cafe.javachobostudy.muk2008;
+
+public class IntToChar {
+    public static void main(String[] args) {
+        char word1 = 'a';
+        char word2 = 97;
+        char word3 = Character.MAX_VALUE;
+        int i1 = 'a';
+        System.out.println(word1);
+        System.out.println(word2);
+        System.out.println(word3);
+        System.out.println(i1);
+    }
+}
