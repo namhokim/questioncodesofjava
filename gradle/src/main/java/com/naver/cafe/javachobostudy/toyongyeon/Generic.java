35c52ba1ed87e1260431f83fa3e77ab9432255fe
namo <nhkimnamo@gmail.com>
2020-02-25 12:29:40 +0900

    자바라는 언어는 객체지향 언어라고 하는데 왜 제네릭이 나왔을까? 제네릭이 우리에게 주는 이점은 무엇인가? 제네릭은 처음부터 꼭 써야 하는것인가?
    List list = Arrays.asList(1,2,3,4);
    이렇게 선언하는 것과
    List<Integer> list = Arrays.asList(1,2,3,4);
    차이점은 제네릭 사용여부 인데...
    제네릭을 쓰면 컴파일 단계에서 에러를 잡아 주니 좋다?
    
    그럼 위 list와 아래 list에 대해서
    공통으로 아래와 같이 넣으면?
    
    에러 없이 잘 진행된다. 컴파일 에러는 나지 않는다.
    그럼 사용시 무리가 없는건가?
    런타임때 값을 빼서 변환할때 오류가 나는건가?
    이런식의 오류는 미래에 대한 오류를 기약하며 짜는 코드인가?
    
    이를 대비하기 위해서 제네릭을 쓰는걸까?
    제네릭을 왜 써야하는지?
    제네릭을 쓰면서 주는 이점?
    제네릭을 효과적으로 사용하는 방법?
    제네릭이 필요한 시기는?
    
    -> UnsupportedOperationException 발생
    
    ref. https://cafe.naver.com/javachobostudy/166284

---
+++ gradle/src/main/java/com/naver/cafe/javachobostudy/toyongyeon/Generic.java
@@ -0,0 +1,12 @@
+package com.naver.cafe.javachobostudy.toyongyeon;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Generic {
+    public static void main(String[] args) {
+        List list = Arrays.asList(1, 2, 3, 4);
+        list.add("1");
+        list.get(0);
+    }
+}
