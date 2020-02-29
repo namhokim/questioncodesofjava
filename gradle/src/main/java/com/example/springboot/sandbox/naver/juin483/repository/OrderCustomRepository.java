ed90e2a7309e93c3b7b0f88051790c93315c2140
Namho Kim <nhkimnamo@gmail.com>
2020-02-29 19:47:31 +0900

    remove OrderCustomRepository

--- gradle/src/main/java/com/example/springboot/sandbox/naver/juin483/repository/OrderCustomRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.springboot.sandbox.naver.juin483.repository;
-
-public interface OrderCustomRepository {
-	void save(Order order);
-
-	Order findOne(Long id);
-}
--- gradle/src/main/java/com/example/springboot/sandbox/naver/juin483/repository/OrderRepository.java
+++ gradle/src/main/java/com/example/springboot/sandbox/naver/juin483/repository/OrderRepository.java
@@ -2,5 +2,5 @@ package com.example.springboot.sandbox.naver.juin483.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface OrderRepository extends JpaRepository<Order, Long>, OrderCustomRepository {
+public interface OrderRepository extends JpaRepository<Order, Long> {
 }
