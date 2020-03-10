57bbf713b5e1c752c0a017d8542767871835c14c
namo <nhkimnamo@gmail.com>
2020-03-10 17:00:10 +0900

    create and add

--- gradle/src/main/java/com/naver/cafe/javachobostudy/farmerkyh/JsonSimple.java
+++ gradle/src/main/java/com/naver/cafe/javachobostudy/farmerkyh/JsonSimple.java
@@ -3,9 +3,11 @@ package com.naver.cafe.javachobostudy.farmerkyh;
 import org.json.simple.JSONArray;
 
 public class JsonSimple {
+    @SuppressWarnings("unchecked")
     public static void main(String[] args) {
         String jsStr= "string형 json 객체 존재";
-        JSONArray array = new JSONArray(jsStr);
+        JSONArray array = new JSONArray();
+        array.add(jsStr);
         System.out.println(array);
     }
 }
