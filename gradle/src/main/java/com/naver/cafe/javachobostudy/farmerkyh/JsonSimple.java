da707f4560a5c88d079af763a9308f22b47170ca
namo <nhkimnamo@gmail.com>
2020-03-10 17:36:25 +0900

    제가 가지고 있는 string값은="[{A:a,B:b,C:,c},{A:a,B:b,C:,c},{A:a,B:b,C:,c},{A:a,B:b,C:,c}...]"이런형식 입니다. 화면에서 위와 같은 값을 받아서 jsonarray객체로 만들려고 했었거든요. 그럼, 위 값을 ArrayList로 쉽게 변환할 수 있을까요? 이게 쉽게 풀릴줄 알았는데..시간만 자꾸 가네요. ㅠㅠ

--- gradle/src/main/java/com/naver/cafe/javachobostudy/farmerkyh/JsonSimple.java
+++ gradle/src/main/java/com/naver/cafe/javachobostudy/farmerkyh/JsonSimple.java
@@ -1,13 +1,20 @@
 package com.naver.cafe.javachobostudy.farmerkyh;
 
+import org.json.JSONObject;
 import org.json.simple.JSONArray;
 
 public class JsonSimple {
     @SuppressWarnings("unchecked")
     public static void main(String[] args) {
-        String jsStr= "string형 json 객체 존재";
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("A", "a");
+        jsonObject.put("B", "b");
+        jsonObject.put("C", "c");
+
         JSONArray array = new JSONArray();
-        array.add(jsStr);
+        array.add(jsonObject);
+        array.add(jsonObject);
+        array.add(jsonObject);
         System.out.println(array);
     }
 }
