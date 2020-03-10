3d08bea416c6b5d890e400c74cfd8ba0d93d16ba
Namho Kim <nhkimnamo@gmail.com>
2020-03-10 22:26:44 +0900

    JSONObject from jsString

--- gradle/src/main/java/com/naver/cafe/javachobostudy/farmerkyh/JsonSimple.java
+++ gradle/src/main/java/com/naver/cafe/javachobostudy/farmerkyh/JsonSimple.java
@@ -1,15 +1,15 @@
 package com.naver.cafe.javachobostudy.farmerkyh;
 
 import org.json.JSONObject;
+import org.json.JSONTokener;
 import org.json.simple.JSONArray;
 
 public class JsonSimple {
     @SuppressWarnings("unchecked")
     public static void main(String[] args) {
-        JSONObject jsonObject = new JSONObject();
-        jsonObject.put("A", "a");
-        jsonObject.put("B", "b");
-        jsonObject.put("C", "c");
+        String jsString = "{\"A\":\"a\",\"B\":\"b\",\"C\":\"c\"}";
+        JSONTokener jsonTokener = new JSONTokener(jsString);
+        JSONObject jsonObject = new JSONObject(jsonTokener);
 
         JSONArray array = new JSONArray();
         array.add(jsonObject);
