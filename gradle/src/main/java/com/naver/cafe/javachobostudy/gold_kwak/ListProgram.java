b3b5048269e62cc42d4d700c9661c8d3d66a6fff
Namho Kim <nhkimnamo@gmail.com>
2020-03-12 22:31:55 +0900

    add array initialize add field initialize test

--- gradle/src/main/java/com/naver/cafe/javachobostudy/gold_kwak/ListProgram.java
+++ gradle/src/main/java/com/naver/cafe/javachobostudy/gold_kwak/ListProgram.java
@@ -1,9 +1,16 @@
 package com.naver.cafe.javachobostudy.gold_kwak;
 
 public class ListProgram {
+	String prog = "field";
+
 	public static void main(String[] args) {
 		ExamList list = new ExamList();
-		list.exams = new Exam[3];
+		list.exams[0] = new Exam();
+		list.exams[1] = new Exam();
+		list.exams[2] = new Exam();
 		list.current = 0;
+
+		ListProgram listProgram = new ListProgram();
+		System.out.println(listProgram.prog);
 	}
 }
