70ce31704741a6b3f01256e8e43e3a3be63fa017
namo <nhkimnamo@gmail.com>
2020-03-13 10:12:03 +0900

    1. 145페이지/예제5-7 설명문 / 설명문에서 마지막 문단에 "매개변수를 입력하지 않으면 크기가 0인 배열이 생성되어 ~~ 앞서 배운 것처럼 이렇게 크기가 0인" >>>>> "매개변수를 입력하지 않으면 길이가 0인 배열이 생성되어 ~~ 앞서 배운 것처럼 이렇게 길이가 0인" 로 바꾸셔야 할 것 같습니다.
    크기: Size
    길이: length
    
    ref. https://cafe.naver.com/javachobostudy/166548

---
+++ gradle/src/main/java/com/naver/cafe/javachobostudy/wea999/ArrayLengthOrSize.java
@@ -0,0 +1,14 @@
+package com.naver.cafe.javachobostudy.wea999;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayLengthOrSize {
+    public static void main(String[] args) {
+        int[] array = new int[] {};
+        System.out.println(array.length);
+
+        List<Integer> integers = new ArrayList<>();
+        System.out.println(integers.size());
+    }
+}
