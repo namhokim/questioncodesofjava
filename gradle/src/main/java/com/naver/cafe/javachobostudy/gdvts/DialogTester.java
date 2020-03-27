35af20c480a82229a132927042a33ffce5091c84
Namho Kim <nhkimnamo@gmail.com>
2020-03-27 21:42:11 +0900

    - 질문 제목 : 자바 실행 에러... - 질문 내용 : 안녕하세요. 자바에 흥미가 있어 가입해 놓고, 일에 치여 살다가 다시 공부를 시작하였습니다. 어제 이클립스 2020.03. 버전과 jdk-14 를 노트북에 설치를 하고, 15년 전쯤에 대학교 강의 때문에 구입한 Big JAVA 라는 책을 가지고, Hello, World! 를 작성 및 실행하였습니다. 그리고 프로그래밍 연습문제를 하나하나 하다가 갑자기 실행이 안 되면서 막히네요. 몇 시간째 매달려 보고 찾아보고 했는데, 안 되서 이렇게 글을 남기게 되었습니다.
    ref. https://cafe.naver.com/javachobostudy/166782

---
+++ gradle/src/main/java/com/naver/cafe/javachobostudy/gdvts/DialogTester.java
@@ -0,0 +1,10 @@
+package com.naver.cafe.javachobostudy.gdvts;
+
+import javax.swing.*;
+
+public class DialogTester {
+	public static void main(String[] args) {
+		JOptionPane.showConfirmDialog(null, "Hello, World!");
+		System.exit(0);
+	}
+}
