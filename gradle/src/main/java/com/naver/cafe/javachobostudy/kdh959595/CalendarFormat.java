61e7520f0527876689ca2825741b705fb1f35f6c
Namho Kim <nhkimnamo@gmail.com>
2020-03-30 23:13:22 +0900

    - 질문 제목 : format적용위해 getTime 이용시 날짜 셋팅이 풀림. - 질문 내용 : 2019년 3월1일부터의 데이터를 가져와야해서 위와같이 작성했는데 startHourDate 값이 현재년월일시간으로 다시 셋팅됩니다. 어떻게 해야할지 말씀 부탁드립니다.
    ref. https://cafe.naver.com/javachobostudy/166831
    
    - 질문 내용 :

--- gradle/src/main/java/com/naver/cafe/javachobostudy/kdh959595/CalendarFormat.java
+++ gradle/src/main/java/com/naver/cafe/javachobostudy/kdh959595/CalendarFormat.java
@@ -4,14 +4,15 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
 public class CalendarFormat {
-	public static final String yyyyMMddHHmmss = "yyyyMMddHHmmss";
+	public static final String YYYYMMDDHHMMSS = "yyyyMMddHHmmss";
 
 	public static void main(String[] args) {
 		Calendar cal = Calendar.getInstance();
-		SimpleDateFormat sdf = new SimpleDateFormat(yyyyMMddHHmmss);
-		cal.set(2019, 2, 1);
+		cal.set(2019, Calendar.MARCH, 1);
+
+		SimpleDateFormat sdf = new SimpleDateFormat(YYYYMMDDHHMMSS);
 		String startHourDate = sdf.format(cal.getTime());
 
-		System.out.println(startHourDate);
+		System.out.println(startHourDate);	// 20190301191437
 	}
 }
