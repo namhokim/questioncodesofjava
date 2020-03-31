06dbc1bb540a4d9b62e74cb602f547084bb9d509
Namho Kim <nhkimnamo@gmail.com>
2020-03-31 21:28:00 +0900

    - 질문 제목 : 배열과 배열의 값을 비교 - 질문 내용 : array를 사용할 때 ==을 사용하면 오류가 많이 떠서 equals를 사용하고,           String 값을 비교할 때 valueOf를 사용하고 있었습니다.           그런데 배열과 배열 간의 값 비교에서는 안 먹히네요. 어떻게 하면 좋을까요ㅎㅎ           답변 항상 정말 감사합니다           - 에러메세지: NPE,Nullpointerexception

---
+++ gradle/src/main/java/com/naver/cafe/javachobostudy/nant0313/ArrayCompare.java
@@ -0,0 +1,14 @@
+package com.naver.cafe.javachobostudy.nant0313;
+
+public class ArrayCompare {
+	public static void main(String[] args) {
+		String abc[] = { "co", "ro", "na" };
+		String bcd[] = { "co", "ro", "na" };
+
+		if(abc[0]==bcd[0])
+		System.out.println("true");
+
+		if(abc[0].equals(String.valueOf(bcd[0])))
+		System.out.println("true");
+	}
+}
