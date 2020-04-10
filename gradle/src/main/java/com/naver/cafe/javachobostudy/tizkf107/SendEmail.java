65cdd5db355e3322c889a7b9baa7ae6397758bcb
김남호/플랫폼실 <nhk@yanolja.com>
2020-04-10 09:42:56 +0900

    구글링 하고 여러가지 시도를 해도 new MimeMessage 생성 부분에서 계속 에러가 나서 도움 받으려고 왔습니다..
    ref. https://cafe.naver.com/javachobostudy/167037

---
+++ gradle/src/main/java/com/naver/cafe/javachobostudy/tizkf107/SendEmail.java
@@ -0,0 +1,58 @@
+package com.naver.cafe.javachobostudy.tizkf107;
+
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+public class SendEmail {
+    public static void main(String[] args) {
+
+        String user = "aaaa@gmail.com";
+        String password = "password";
+
+        // SMTP 서버 정보 설정
+        Properties prop = new Properties();
+        prop.put("mail.smtp.host", "smtp.gmail.com");
+        prop.put("mail.smtp.port", 465);
+        prop.put("mail.smtp.auth", "true");
+        prop.put("mail.smtp.ssl.enable", "true");
+        prop.put("mail.smtp.ssl.trust", "smtp.gmail.com");
+
+        Session session = Session.getDefaultInstance(prop, new javax.mail.Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(user, password);
+            }
+        });
+
+        try {
+            MimeMessage message = new MimeMessage(session); // <-- 여기서 에러 납니다..
+            message.setFrom(new InternetAddress(user));
+
+            //수신자메일주소
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress("aaaa@naver.com"));
+
+            // Subject
+            message.setSubject("제목을 입력하세요"); //메일 제목을 입력
+
+            // Text
+            message.setText("내용을 입력하세요");    //메일 내용을 입력
+
+            // send the message
+            Transport.send(message); ////전송
+            System.out.println("message sent successfully...");
+        } catch (AddressException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (MessagingException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+}
