72c7cb2180127aec782751a11a0bf26f57272fb4
namhokim <nhkimnamo@gmail.com>
2020-05-04 23:42:27 +0900

    - 질문 제목 : 스레드풀 관련 질문드립니다. - 질문 내용 : 스레드풀 관련해서 다름이아니라 궁금한게 있어서 질문드립니다. 제가 알기로는 인터페이스 변수가 반환 타입이면 그 인터페이스를 구현한 클래스의 인스턴스를 반환 받는걸로 알고있는데요. 참 헷갈리는게 ExecutorService 인터페이스의 execute 메소드는 리턴 값이 void고 아무런 기능을 하는 게 없는데 어떻게 작업 큐에 작업을 올리는 역할을 어디서 받아오는건지 궁금하고, shutdown 메소드도 마찬가지 질문 입니다. 그냥 추상메서드 하나 선언 되어있는게 전부고 어떤 클래스에서 종료를 하는 기능을 받아오는 구문도 없는데 어떻게 추상메서드 하나로 종료를 하는 기능을 내포하고 있는지 답변 좀 부탁 드립니다.
    ref. https://cafe.naver.com/javachobostudy/167480

---
+++ gradle/src/main/java/com/naver/cafe/javachobostudy/dlawnstn12345/Ex.java
@@ -0,0 +1,33 @@
+package com.naver.cafe.javachobostudy.dlawnstn12345;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+
+public class Ex {
+    public static void main(String[] args) {
+
+        ExecutorService exe = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+
+        for(int i=0; i<10; i++) {
+            Runnable run = () -> {
+                ThreadPoolExecutor threadPoolExecutor = (ThreadPoolExecutor) exe;
+
+                int poolsize = threadPoolExecutor.getPoolSize();
+                String threadName = Thread.currentThread().getName();
+                System.out.println("총 스레드 개수 : " + poolsize + "작업스레드 이름 : " + threadName);
+
+                //int value = Integer.parseInt("숫자");
+            };
+
+            exe.execute(run);
+
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {}
+
+
+        }
+        exe.shutdown();
+    }
+}
