85e27a59a9b9e1debf5c1df1c04b88aa397d8b15
namo <nhkimnamo@gmail.com>
2020-05-04 15:09:40 +0900

    리팩토링 - 주어진 범위 출력 method extract - 메시지 양식 변경: 범위는 한번만, 마지막 콤마 표시 안함 - dangling if, for 방지용 중괄호 추가

--- gradle/src/main/java/com/naver/cafe/javachobostudy/anwls1634/FindPrimes.java
+++ gradle/src/main/java/com/naver/cafe/javachobostudy/anwls1634/FindPrimes.java
@@ -1,53 +1,57 @@
 package com.naver.cafe.javachobostudy.anwls1634;
+
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class FindPrimes {
+
+    private static final String INPUT_MESSAGE = "Enter two integers greater than 1: ";
+
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter two integers larger than 1: : ");
-
         while (true) {
+            System.out.print(INPUT_MESSAGE);
             try {
                 int a = sc.nextInt();
                 int b = sc.nextInt();
-                int i;
-                int c, d;
-                if (a > b) {
-                    d = a;
-                    c = b;
 
-                } else {
-                    c = a;
-                    d = b;
-                }
-                for (i = c; i <= d; i++) {
-                    if (isPrime(i))
-                        System.out.print("Primes between [" + (int) c + ", " + (int) d + "] = "+i+", ");
-                }
-            } catch (InputMismatchException ime) {
+                printPrimesBetween(a, b);
+            } catch (InputMismatchException | NumberFormatException ime) {
                 sc = new Scanner(System.in);
                 System.out.println("Wrong input values!");
-                System.out.print("Enter two integers larger than 1: ");
-            } catch (NumberFormatException nfe) {
-                sc = new Scanner(System.in);
-                System.out.println("Wrong input values!");
-                System.out.print("Enter two integers larger than 1: ");
+                System.out.print(INPUT_MESSAGE);
             }
-
         }
     }
 
+    static void printPrimesBetween(int from, int to) {
+        int lower = Math.min(from, to);
+        int higher = Math.max(from, to);
+        boolean hasBeforePrimeNumber = false;
+
+        System.out.print("Primes between [" + lower + ", " + higher + "] = ");
+        for (int i = lower; i <= higher; i++) {
+            if (isPrime(i)) {
+                if (hasBeforePrimeNumber) {
+                    System.out.print(", ");
+                }
+                System.out.print(i);
+                hasBeforePrimeNumber = true;
+            }
+        }
+        System.out.println();
+    }
+
     static boolean isPrime(int n) {
-        int i;
-
-        if (n == 1)
+        if (n == 1) {
             return false;
+        }
 
-        for (i = 2; i < n; i++)
-            if (n % i == 0)
+        for (int i = 2; i < n; i++) {
+            if (n % i == 0) {
                 return false;
+            }
+        }
 
         return true;
     }
