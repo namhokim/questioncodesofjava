0ff8e7b7e50f2cc5d123d4fc2abb473317da7397
namhokim <nhkimnamo@gmail.com>
2020-05-05 19:27:58 +0900

    - 질문 제목 : 강의를 보던중 Math.random메서드에 에러가 발생햇습니다 - 질문 내용 : 메서드에 에러(빨간줄)가 뜬적이 많았는데, 실행이되서 그냥 했었습니다. 근데 이건 실행도 안되서 글씁니다.
    import java.until.*; 했습니다. 강의랑 똑같이 썻는데 에러가 뜨네요 ㅠㅠ
    
    - 에러메세지:  The method random() is undefined for the type math.

--- gradle/src/main/java/com/naver/cafe/javachobostudy/dlawnstn12345/Ex.java
+++ gradle/src/main/java/com/naver/cafe/javachobostudy/dlawnstn12345/Ex.java
@@ -6,16 +6,15 @@ import java.util.concurrent.ThreadPoolExecutor;
 
 public class Ex {
     public static void main(String[] args) {
-
         ExecutorService exe = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
 
-        for(int i=0; i<10; i++) {
+        for (int i = 0; i < 100; i++) {
             Runnable run = () -> {
                 ThreadPoolExecutor threadPoolExecutor = (ThreadPoolExecutor) exe;
 
                 int poolsize = threadPoolExecutor.getPoolSize();
                 String threadName = Thread.currentThread().getName();
-                System.out.println("총 스레드 개수 : " + poolsize + "작업스레드 이름 : " + threadName);
+                System.out.println("총 스레드 개수 : " + poolsize + ", 작업스레드 이름 : " + threadName);
 
                 //int value = Integer.parseInt("숫자");
             };
@@ -24,9 +23,8 @@ public class Ex {
 
             try {
                 Thread.sleep(10);
-            } catch (InterruptedException e) {}
-
-
+            } catch (InterruptedException e) {
+            }
         }
         exe.shutdown();
     }
---
+++ gradle/src/main/java/com/naver/cafe/javachobostudy/wlsdk6226/MathRandom.java
@@ -0,0 +1,10 @@
+package com.naver.cafe.javachobostudy.wlsdk6226;
+
+import java.util.*;
+
+public class MathRandom {
+    public static void main(String[] args) {
+        double rnd = Math.random();
+        System.out.println(rnd);
+    }
+}
