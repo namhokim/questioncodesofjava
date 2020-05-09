ccea974e2f57b6ebe9727f7d39cfd77f53058ee4
namhokim <nhkimnamo@gmail.com>
2020-05-09 19:59:54 +0900

    reverse

--- gradle/src/main/java/com/naver/cafe/javachobostudy/jin78237/StarPrint.java
+++ gradle/src/main/java/com/naver/cafe/javachobostudy/jin78237/StarPrint.java
@@ -2,10 +2,13 @@ package com.naver.cafe.javachobostudy.jin78237;
 
 public class StarPrint {
     public static void main(String[] args) {
-        int i,j;
-        for(i=0; i<7; i++) {
-            for(j=0; j<=i; j++) {
-                System.out.printf("%d", j);
+        for (int i = 0; i < 7; i++) {
+            int j;
+            for (j = 0; j <= i; j++) {
+                System.out.print(j);
+            }
+            for (j = j - 1; j > 0; ) {
+                System.out.print(--j);
             }
             System.out.println();
         }
