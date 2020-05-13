b94ddf870580f4cb17fa0c78a1b4e30a10d7d801
namhokim <nhkimnamo@gmail.com>
2020-05-13 19:58:53 +0900

    100개의 난수를 직접 저장하는 대신 생성된 난수의 카운트를 갱신함. - int[100] 불필요 - 카운트를 위한 순회 불필요

--- gradle/src/main/java/com/naver/cafe/javachobostudy/jin78237/MaxFrequencyRandom.java
+++ gradle/src/main/java/com/naver/cafe/javachobostudy/jin78237/MaxFrequencyRandom.java
@@ -1,14 +1,38 @@
 package com.naver.cafe.javachobostudy.jin78237;
 
+import java.util.stream.IntStream;
+
 public class MaxFrequencyRandom {
+
+    private static final int SIZE_OF_RANGE = 10;
+
     public static void main(String[] args) {
-        int[] no = new int[100];
+        int[] counts = new int[SIZE_OF_RANGE];
 
         for (int i = 0; i < 100; i++) {
-            no[i] = (int) (Math.random() * 10) + 1;
+            int generatedNumber = (int) (Math.random() * SIZE_OF_RANGE) + 1;
+            counts[generatedNumber - 1]++;
         }
-        for (int i = 0; i < no.length; i++) {
-            System.out.printf("%4d", no[i]);
+
+        System.out.print("정수:");
+        IntStream.rangeClosed(1, SIZE_OF_RANGE).forEach(x -> System.out.printf(" %d", x));
+        System.out.println();
+
+        int maxFreqNumber = 0;
+        int maxFreqCount = counts[0];
+        System.out.print("발생횟수:");
+        for (int i = 0; i < SIZE_OF_RANGE; i++) {
+            int count = counts[i];
+
+            System.out.print(' ');
+            System.out.print(count);
+            if (count > maxFreqCount) {
+                maxFreqNumber = i;
+                maxFreqCount = count;
+            }
         }
+        System.out.println();
+
+        System.out.printf("정수: %d, 횟수: %d%n", maxFreqNumber + 1, maxFreqCount);
     }
 }
