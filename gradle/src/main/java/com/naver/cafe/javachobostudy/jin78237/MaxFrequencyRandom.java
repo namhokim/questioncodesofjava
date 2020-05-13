e77b805020fe059b030903d754274e0d651d0d94
namhokim <nhkimnamo@gmail.com>
2020-05-13 19:34:12 +0900

    formatting

--- gradle/src/main/java/com/naver/cafe/javachobostudy/jin78237/MaxFrequencyRandom.java
+++ gradle/src/main/java/com/naver/cafe/javachobostudy/jin78237/MaxFrequencyRandom.java
@@ -2,9 +2,9 @@ package com.naver.cafe.javachobostudy.jin78237;
 
 public class MaxFrequencyRandom {
     public static void main(String[] args) {
-        int[]no = new int[100];
+        int[] no = new int[100];
 
-        for(int i = 0 ;i<100; i++) {
+        for (int i = 0; i < 100; i++) {
             no[i] = (int) (Math.random() * 10) + 1;
         }
         for (int i = 0; i < no.length; i++) {
