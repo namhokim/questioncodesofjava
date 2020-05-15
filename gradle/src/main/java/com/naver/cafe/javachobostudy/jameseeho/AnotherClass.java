d8c26cae8a56214d353c0048bb73f6c7fc3ae62d
namhokim <nhkimnamo@gmail.com>
2020-05-15 19:55:33 +0900

    static 메서드로 변경하여 객체를 만드는 클래스의 인스턴스를 생성하지 않도록 변경

--- gradle/src/main/java/com/naver/cafe/javachobostudy/jameseeho/AnotherClass.java
+++ gradle/src/main/java/com/naver/cafe/javachobostudy/jameseeho/AnotherClass.java
@@ -1,7 +1,7 @@
 package com.naver.cafe.javachobostudy.jameseeho;
 
 public class AnotherClass {
-    public Tv createIntance() {
+    public static Tv createIntance() {
         Tv tv = new Tv();
         tv.channel = 2;
         return tv;
--- gradle/src/main/java/com/naver/cafe/javachobostudy/jameseeho/Main.java
+++ gradle/src/main/java/com/naver/cafe/javachobostudy/jameseeho/Main.java
@@ -2,8 +2,7 @@ package com.naver.cafe.javachobostudy.jameseeho;
 
 public class Main {
     public static void main(String[] args) {
-        AnotherClass anotherClass = new AnotherClass();
-        final Tv intance = anotherClass.createIntance();
+        final Tv intance = AnotherClass.createIntance();
         System.out.println(intance.channel);
     }
 }
