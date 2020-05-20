30cc661aa5d473b784244e2362ed10d8070510dd
namhokim <nhkimnamo@gmail.com>
2020-05-20 16:32:13 +0900

    - 질문 제목 :  래핑 언래핑에 대한 질문 - 질문 내용 :   안녕하세요. 오늘 자바 래핑 언래핑에 대해서 배웠는데요.
    아예 감이 안잡혀서 이렇게 글을 올리게 되었습니다.
    객체에 둘러싸이는게 래핑 객체에서 풀려나오는게 언래핑이라는건 알겠는데 이걸 막상 코드로써 표현을 하려하면
    머리가 백지가 되네요 ..
    좋은 예제나 설명 부탁드립니다 ㅠㅠ
    
    ref. https://cafe.naver.com/javachobostudy/167765

---
+++ gradle/src/main/java/com/naver/cafe/javachobostudy/journy002/AutoBoxingUnboxing.java
@@ -0,0 +1,11 @@
+package com.naver.cafe.javachobostudy.journy002;
+
+public class AutoBoxingUnboxing {
+    public static void main(String[] args) {
+        Integer integer = 12345;    // auto boxing
+        int i = integer;            // auto unboxing
+
+        System.out.println(integer);
+        System.out.println(i);
+    }
+}
