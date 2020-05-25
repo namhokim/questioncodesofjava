342e7006ddc9cd7126a631a391e1f674530b5c35
namhokim <nhkimnamo@gmail.com>
2020-05-25 13:48:15 +0900

    내부 구현체로 ArrayList를 선택

--- gradle/src/main/java/com/naver/cafe/javachobostudy/qkrdlfrnjs95/ListEx.java
+++ gradle/src/main/java/com/naver/cafe/javachobostudy/qkrdlfrnjs95/ListEx.java
@@ -1,24 +1,31 @@
 package com.naver.cafe.javachobostudy.qkrdlfrnjs95;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class ListEx {
-    MyList<String> list1 = new MyList<String>();
-    String strArr[] = {"java","Database","Network","Python"};
-    list1.set(strArr); //이부분 오류.
-    list1.printList();
+    public static void main(String[] args) {
+        MyList<String> list1 = new MyList<>();
+        String[] strArr = {"java", "Database", "Network", "Python"};
+        list1.set(strArr);
+        list1.printList();
+    }
 }
 
-class MyList<T>{
-    T val;
+class MyList<T> {
+    List<T> values;
 
-    MyList(){
+    MyList() {
+        values = new ArrayList<>(); // 내부 구현체로 ArrayList를 사용
     }
 
-    void set(T a) {
-        val = a;
+    void set(T[] aray) {
+        values.addAll(Arrays.asList(aray));
     }
 
-    void printList(){
-        System.out.println(val);
+    void printList() {
+        System.out.println(values);
         System.out.println("--------------------------");
     }
 }
