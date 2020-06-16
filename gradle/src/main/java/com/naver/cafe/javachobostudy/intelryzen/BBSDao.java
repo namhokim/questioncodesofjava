9decf772f157a155063c752f2da6c56fb668f100
namhokim <nhkimnamo@gmail.com>
2020-06-17 01:36:26 +0900

    2. DB의 시계에 의존하지 않고 Application 의 시스템 시간으로 설정한다.

--- gradle/src/main/java/com/naver/cafe/javachobostudy/intelryzen/BBSDao.java
+++ gradle/src/main/java/com/naver/cafe/javachobostudy/intelryzen/BBSDao.java
@@ -4,43 +4,34 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class BBSDao {
+    /**
+     * eg) 2020-06-16 16:30:49
+     */
+    private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
     private Connection conn = null;
-    private PreparedStatement pstmt = null;
-    private ResultSet rs = null;
 
     public BBSDao() {
         try {
             String dbUrl = "jdbc:mysql://localhost:3306/BBS";
             String dbId = "root";
             String dbPassword = "1234";
-            Class.forName("com.mysql.jdbc.Driver");
+            Class.forName("com.mysql.cj.jdbc.Driver");
             conn = DriverManager.getConnection(dbUrl, dbId, dbPassword);
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    public String getDate() {
-        String query = "select NOW()";
-        try {
-            pstmt = conn.prepareStatement(query);
-            rs = pstmt.executeQuery();
-            if (rs.next()) {
-                return rs.getString(1);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return "";
-    }
-
     public int getNext() {
         String query = "select bbsId from BBS order by bbsId desc";
         try {
-            pstmt = conn.prepareStatement(query);
-            rs = pstmt.executeQuery();
+            PreparedStatement pstmt = conn.prepareStatement(query);
+            ResultSet rs = pstmt.executeQuery();
             if (rs.next()) {
                 return rs.getInt(1) + 1;
             }
@@ -54,11 +45,11 @@ public class BBSDao {
     public int write(String bbsTitle, String bbsContent, String userId) {
         String query = "insert into BBS values(?, ?, ?, ?, ?, ?)";
         try {
-            pstmt = conn.prepareStatement(query);
+            PreparedStatement pstmt = conn.prepareStatement(query);
             pstmt.setInt(1, getNext());
             pstmt.setString(2, bbsTitle);
             pstmt.setString(3, userId);
-            pstmt.setString(4, getDate());
+            pstmt.setString(4, now());
             pstmt.setString(5, bbsContent);
             pstmt.setInt(6, 1);
             return pstmt.executeUpdate();
@@ -66,6 +57,10 @@ public class BBSDao {
             e.printStackTrace();
         }
         return -5;
+    }
+
+    private String now() {
+        return LocalDateTime.now().format(DATE_TIME_FORMATTER);
     }
 
     public static void main(String[] args) {
