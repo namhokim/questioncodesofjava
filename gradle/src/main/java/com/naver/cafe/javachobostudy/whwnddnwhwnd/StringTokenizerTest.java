2affd5f12f9dd708946bdc512d1e79571422be2a
namo <nhkimnamo@gmail.com>
2020-06-16 17:37:49 +0900

    - 질문 제목 : StringTokenizer로 입력받은 값이 제대로 출력되지 않습니다. - 에러메세지: 여기서 num에 1,2,3,4 를 입력해서 arr[0] = 1 / arr[1] = 2 /arr[2] = 3 /arr[3] = 4 이렇게 저장해서 출력할때 1 2 3 4
    이렇게 출력하고싶은데
    
    1
    2
    까지밖에 나오지 않습니다.. 이유가 뭘까요..
    
    ref. https://cafe.naver.com/javachobostudy/168282

---
+++ gradle/src/main/java/com/naver/cafe/javachobostudy/whwnddnwhwnd/StringTokenizerTest.java
@@ -0,0 +1,21 @@
+package com.naver.cafe.javachobostudy.whwnddnwhwnd;
+
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class StringTokenizerTest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("입력");
+        String num = sc.nextLine();
+
+        StringTokenizer str = new StringTokenizer(num, ",");
+
+        int[] arr = new int[1000];
+        System.out.println("출력");
+        for (int i = 0; i < str.countTokens(); i++) {
+            arr[i] = Integer.parseInt(str.nextToken());
+            System.out.println(arr[i]);
+        }
+    }
+}
