64f7894c2aac4d0d7377762e278d749130f35dba
namhokim <nhkimnamo@gmail.com>
2020-06-17 01:47:39 +0900

    3. getNext() 리팩토링 - 쿼리의 수정 (단건만 알면 된다) - 매직넘버의 상수화 - 알 수 없는 리턴값의 제거(NextSequenceGenerationException 예외로 변경) - prepareStatement 대신 createStatement 로 변경 (파라미터 없는 쿼리)

--- gradle/src/main/java/com/naver/cafe/javachobostudy/intelryzen/BBSDao.java
+++ gradle/src/main/java/com/naver/cafe/javachobostudy/intelryzen/BBSDao.java
@@ -1,17 +1,24 @@
 package com.naver.cafe.javachobostudy.intelryzen;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
+import com.google.common.base.Preconditions;
+
+import java.sql.*;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Objects;
+
+import static com.google.common.base.Preconditions.*;
 
 public class BBSDao {
     /**
      * eg) 2020-06-16 16:30:49
      */
     private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+    /**
+     * 게시판(BBS)의 ID는 1부터 시작한다고 가정한다.
+     */
+    public static final int DEFAULT_BBS_ID = 1;
+    public static final int FIRST_COLUMN = 1;
 
     private Connection conn = null;
 
@@ -28,18 +35,18 @@ public class BBSDao {
     }
 
     public int getNext() {
-        String query = "select bbsId from BBS order by bbsId desc";
+        String query = "select ifnull(max(bbsId), 0) as nextId from BBS";
         try {
-            PreparedStatement pstmt = conn.prepareStatement(query);
-            ResultSet rs = pstmt.executeQuery();
-            if (rs.next()) {
-                return rs.getInt(1) + 1;
+            final Statement statement = conn.createStatement();
+            if (!statement.execute(query)) {
+                return DEFAULT_BBS_ID;
             }
-            return 1;
+            final ResultSet resultSet = statement.getResultSet();
+            checkState(resultSet.next(), "쿼리 수행 결과가 반드시 한 개 존재해야 합니다.");
+            return resultSet.getInt(FIRST_COLUMN) + 1;
         } catch (Exception e) {
-            e.printStackTrace();
+            throw new NextSequenceGenerationException(e);
         }
-        return -4;// 데이터베이스 오류
     }
 
     public int write(String bbsTitle, String bbsContent, String userId) {
@@ -61,6 +68,15 @@ public class BBSDao {
 
     private String now() {
         return LocalDateTime.now().format(DATE_TIME_FORMATTER);
+    }
+
+    /**
+     * 다음 bbsId 를 가져오지 못했을때 발생하는 예외
+     */
+    public static class NextSequenceGenerationException extends RuntimeException {
+        public NextSequenceGenerationException(Throwable cause) {
+            super(cause);
+        }
     }
 
     public static void main(String[] args) {
