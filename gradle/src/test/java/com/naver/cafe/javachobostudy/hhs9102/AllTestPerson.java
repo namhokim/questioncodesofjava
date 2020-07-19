6e5d6b6a17f6bdb684e1ad8a228c98326db8ed8d
namhokim <nhkimnamo@gmail.com>
2020-07-19 17:26:58 +0900

    A. 만약 contains로 존재여부만 판단하는 것이 아닌, list1과 list2의 객체 정보를 모두 활용한다면 동일하게 Set으로 for를 실행해야 될 것 같은데요.    아래 첨부한 소스에서 list1의 p1 객체와, list2의 p2 객체 정보를 활용해야 한다면 더 나은 방법이 있을까요?

--- gradle/src/test/java/com/naver/cafe/javachobostudy/hhs9102/AllTestPerson.java
+++ gradle/src/test/java/com/naver/cafe/javachobostudy/hhs9102/AllTestPerson.java
@@ -2,8 +2,11 @@ package com.naver.cafe.javachobostudy.hhs9102;
 
 import org.junit.Before;
 import org.junit.Test;
-import java.util.Arrays;
-import java.util.List;
+
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
 import static org.junit.Assert.assertEquals;
 
 public class AllTestPerson {
@@ -11,26 +14,33 @@ public class AllTestPerson {
     List<Person> list2;
 
     @Before
-    public void before(){
-        list1 = Arrays.asList(new Person("철수","김"), new Person("길동","홍"));
-        list2 = Arrays.asList(new Person("철수","방"));
+    public void before() {
+        list1 = Arrays.asList(new Person("철수", "김"), new Person("길동", "홍"));
+        list2 = Arrays.asList(new Person("철수", "방"));
     }
 
     @Test
-    public void test(){
-        for(Person p1 : list1){
-            for(Person p2 : list2){
-                if(p1.getFirstName().equals(p2.firstName)){
-//p1, p2의 객체 활용
-                    assertEquals("김철수", p1.getLastName()+p2.getFirstName());
+    public void test() {
+        Map<String, List<Person>> firstNameAndPersonsMap = new HashMap<>(list1.size());
+        for (Person p1 : list1) {
+            List<Person> personList = firstNameAndPersonsMap.getOrDefault(p1.firstName, new ArrayList<>());
+            personList.add(p1);
+            firstNameAndPersonsMap.put(p1.firstName, personList);
+        }
+
+        for (Person p2 : list1) {
+            if (firstNameAndPersonsMap.containsKey(p2.firstName)) {
+                List<Person> personList = firstNameAndPersonsMap.get(p2.firstName);
+                for (Person p1: personList) {
+                    assertEquals(p1.firstName + p1.lastName, p2.firstName + p2.lastName);
                 }
             }
         }
     }
 
-    public class Person{
-        String firstName;
-        String lastName;
+    public static class Person {
+        private String firstName;
+        private String lastName;
 
         public Person(String firstName, String lastName) {
             this.firstName = firstName;
@@ -43,6 +53,24 @@ public class AllTestPerson {
 
         public String getLastName() {
             return lastName;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
+            Person person = (Person) o;
+            return Objects.equals(firstName, person.firstName) &&
+                    Objects.equals(lastName, person.lastName);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(firstName, lastName);
         }
     }
 }
