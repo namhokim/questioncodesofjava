7d56f229a2dfc2058498a974dd3386b69be95c6e
namo <nhkimnamo@gmail.com>
2020-07-21 12:21:32 +0900

    Rectangle: 이름을 추가

--- gradle/src/main/java/com/naver/cafe/javachobostudy/asj515/Rectangle.java
+++ gradle/src/main/java/com/naver/cafe/javachobostudy/asj515/Rectangle.java
@@ -1,8 +1,13 @@
 package com.naver.cafe.javachobostudy.asj515;
 
 public class Rectangle {
+    private final String name;  // 이름 프로퍼티 추가
     private final int length; // setter (setLength 메서드)를 지우면 immutable 가능하므로 final 가능
     private final int width;  // setter (setWidth 메서드)를 지우면 immutable 가능하므로 final 가능
+
+    public String getName() {
+        return name;
+    }
 
     public int getLength() {
         return length;
@@ -12,13 +17,14 @@ public class Rectangle {
         return width;
     }
 
-    private Rectangle(int length, int width) {
+    private Rectangle(String name, int length, int width) {
+        this.name = name;
         this.length = length;
         this.width = width;
     }
 
-    public static Rectangle of(int length, int width) {
-        return new Rectangle(length, width);
+    public static Rectangle of(String name, int length, int width) {
+        return new Rectangle(name, length, width);
     }
 
     public int getArea() {
--- gradle/src/main/java/com/naver/cafe/javachobostudy/asj515/Rectangle2.java
+++ gradle/src/main/java/com/naver/cafe/javachobostudy/asj515/Rectangle2.java
@@ -2,11 +2,11 @@ package com.naver.cafe.javachobostudy.asj515;
 
 public class Rectangle2 {
     public static void main(String[] args) {
-        Rectangle kitchen = Rectangle.of(20, 15);
-        Rectangle livingRoom = Rectangle.of(20, 20); // 거실은 living room 으로 복합 단어이므로 R로 단어를 구분
-        Rectangle den = Rectangle.of(15, 10);
-        Rectangle bedroom = Rectangle.of(18, 15);
-        Rectangle bedroom2 = Rectangle.of(10, 8);
+        Rectangle kitchen = Rectangle.of("kitchen", 20, 15);
+        Rectangle livingRoom = Rectangle.of("living room", 20, 20); // 거실은 living room 으로 복합 단어이므로 R로 단어를 구분
+        Rectangle den = Rectangle.of("den", 15, 10);
+        Rectangle bedroom = Rectangle.of("bedroom 1", 18, 15);
+        Rectangle bedroom2 = Rectangle.of("bedroom 2", 10, 8);
 
         Rectangle[] rooms = new Rectangle[5];
         rooms[0] = kitchen;
