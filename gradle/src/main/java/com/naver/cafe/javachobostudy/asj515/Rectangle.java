e413903c87a3e0e3825b5cf73750ef6d880ed71f
namo <nhkimnamo@gmail.com>
2020-07-21 12:03:03 +0900

    Rectangle: - setter를 제거하여 immutable 클래스로 변경 - 생성자에 full name 을 사용 - 불필요한 classroom 멤버 변수를 제거
    Rectangle2:
    - livingroom -> livingRoom
    - Rectangle[] 의 동일 인덱스 0 배정을 1 ~ 4로 변경
    - for 문을 개선된 for 문으로 변경
    - = + 를 += 연산자로 변경

--- gradle/src/main/java/com/naver/cafe/javachobostudy/asj515/Rectangle.java
+++ gradle/src/main/java/com/naver/cafe/javachobostudy/asj515/Rectangle.java
@@ -1,30 +1,21 @@
 package com.naver.cafe.javachobostudy.asj515;
 
 public class Rectangle {
-    private int length;
-    private int width;
+    private final int length; // setter (setLength 메서드)를 지우면 immutable 가능하므로 final 가능
+    private final int width;  // setter (setWidth 메서드)를 지우면 immutable 가능하므로 final 가능
 
     public int getLength() {
         return length;
     }
 
-    public void setLength(int len) {
-        length = len;
-    }
-
     public int getWidth() {
         return width;
     }
-
-    public void setWidth(int wid) {
-        width = wid;
+    public Rectangle(int length, int width) {    // 축약된 len, wid 보다는 전체 단어가 이해하기 쉬움
+        this.length = length;   // 멤버변수와 이름이 겹치므로 this.length 로 구별해줌
+        this.width = width;     // 멤버변수와 이름이 겹치므로 this.width 로 구별해줌
     }
 
-    public Rectangle(int len, int wid) {
-        length = len;
-        width = wid;
-    }
-    Rectangle classroom = new Rectangle(15,20);
     public int getArea() {
         return length * width;
     }
--- gradle/src/main/java/com/naver/cafe/javachobostudy/asj515/Rectangle2.java
+++ gradle/src/main/java/com/naver/cafe/javachobostudy/asj515/Rectangle2.java
@@ -2,24 +2,26 @@ package com.naver.cafe.javachobostudy.asj515;
 
 public class Rectangle2 {
     public static void main(String[] args) {
-        Rectangle kitchen = new Rectangle(20,15);
-        Rectangle livingroom = new Rectangle(20,20);
-        Rectangle den = new Rectangle(15,10);
-        Rectangle bedroom = new Rectangle(18,15);
-        Rectangle bedroom2 = new Rectangle(10,8);
+        Rectangle kitchen = new Rectangle(20, 15);
+        Rectangle livingRoom = new Rectangle(20, 20); // 거실은 living room 으로 복합 단어이므로 R로 단어를 구분
+        Rectangle den = new Rectangle(15, 10);
+        Rectangle bedroom = new Rectangle(18, 15);
+        Rectangle bedroom2 = new Rectangle(10, 8);
 
         Rectangle[] rooms = new Rectangle[5];
         rooms[0] = kitchen;
-        rooms[0] = livingroom;
-        rooms[0] = den;
-        rooms[0] = bedroom;
-        rooms[0] = bedroom2;
+        rooms[1] = livingRoom;  // 배열 첨자 0 -> 1
+        rooms[2] = den;         // 배열 첨자 0 -> 2
+        rooms[3] = bedroom;     // 배열 첨자 0 -> 3
+        rooms[4] = bedroom2;    // 배열 첨자 0 -> 4
+
+        System.out.println(computeArea(rooms));
     }
 
-    public int computeArea(Rectangle[] rooms) {
+    public static int computeArea(Rectangle[] rooms) {  // static method로 변경
         int squarefoot = 0;
-        for (int r=0;r<rooms.length; r++) {
-            squarefoot = squarefoot + rooms[r].getArea();
+        for (Rectangle room : rooms) { // enhanced for loop으로 변경
+            squarefoot += room.getArea(); // x = x + y 는 x += y 로 변경 가능
         }
         return squarefoot;
     }
