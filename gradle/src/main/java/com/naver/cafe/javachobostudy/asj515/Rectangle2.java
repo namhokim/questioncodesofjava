23e7f74647be304f0df2c7993731d2a148223e76
namo <nhkimnamo@gmail.com>
2020-07-21 12:30:20 +0900

    computeArea 의 입력을 배열 대신 Iterable 로 변경

--- gradle/src/main/java/com/naver/cafe/javachobostudy/asj515/Rectangle2.java
+++ gradle/src/main/java/com/naver/cafe/javachobostudy/asj515/Rectangle2.java
@@ -1,5 +1,8 @@
 package com.naver.cafe.javachobostudy.asj515;
 
+import java.util.Arrays;
+import java.util.Iterator;
+
 public class Rectangle2 {
     public static void main(String[] args) {
         Rectangle kitchen = Rectangle.of("kitchen", 20, 15);
@@ -14,11 +17,10 @@ public class Rectangle2 {
         rooms[2] = den;         // 배열 첨자 0 -> 2
         rooms[3] = bedroom;     // 배열 첨자 0 -> 3
         rooms[4] = bedroom2;    // 배열 첨자 0 -> 4
-
-        System.out.println(computeArea(rooms));
+        System.out.println(computeArea(Arrays.asList(rooms)));
     }
 
-    public static int computeArea(Rectangle[] rooms) {  // static method로 변경
+    public static int computeArea(Iterable<Rectangle> rooms) {  // static method로 변경
         int squarefoot = 0;
         for (Rectangle room : rooms) { // enhanced for loop으로 변경
             squarefoot += room.getArea(); // x = x + y 는 x += y 로 변경 가능
