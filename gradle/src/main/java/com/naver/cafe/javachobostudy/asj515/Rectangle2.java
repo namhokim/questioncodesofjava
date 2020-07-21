16d882aa36de56d0c8875f8bbda5aa2eb301fbd7
namo <nhkimnamo@gmail.com>
2020-07-21 12:31:11 +0900

    배열 대신 List로 변경

--- gradle/src/main/java/com/naver/cafe/javachobostudy/asj515/Rectangle2.java
+++ gradle/src/main/java/com/naver/cafe/javachobostudy/asj515/Rectangle2.java
@@ -1,23 +1,18 @@
 package com.naver.cafe.javachobostudy.asj515;
 
-import java.util.Arrays;
-import java.util.Iterator;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Rectangle2 {
     public static void main(String[] args) {
-        Rectangle kitchen = Rectangle.of("kitchen", 20, 15);
-        Rectangle livingRoom = Rectangle.of("living room", 20, 20); // 거실은 living room 으로 복합 단어이므로 R로 단어를 구분
-        Rectangle den = Rectangle.of("den", 15, 10);
-        Rectangle bedroom = Rectangle.of("bedroom 1", 18, 15);
-        Rectangle bedroom2 = Rectangle.of("bedroom 2", 10, 8);
+        List<Rectangle> rooms = new ArrayList<>();
+        rooms.add(Rectangle.of("kitchen", 20, 15));
+        rooms.add(Rectangle.of("living room", 20, 20));
+        rooms.add(Rectangle.of("den", 15, 10));
+        rooms.add(Rectangle.of("bedroom 1", 18, 15));
+        rooms.add(Rectangle.of("bedroom 2", 10, 8));
 
-        Rectangle[] rooms = new Rectangle[5];
-        rooms[0] = kitchen;
-        rooms[1] = livingRoom;  // 배열 첨자 0 -> 1
-        rooms[2] = den;         // 배열 첨자 0 -> 2
-        rooms[3] = bedroom;     // 배열 첨자 0 -> 3
-        rooms[4] = bedroom2;    // 배열 첨자 0 -> 4
-        System.out.println(computeArea(Arrays.asList(rooms)));
+        System.out.println(computeArea(rooms));
     }
 
     public static int computeArea(Iterable<Rectangle> rooms) {  // static method로 변경
