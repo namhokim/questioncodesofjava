170f29350d02683e56b144f7fb949c70ebff2db6
namo <nhkimnamo@gmail.com>
2020-08-13 13:30:45 +0900

    ref. https://cafe.naver.com/thisisjava/26246
    답은 (lengthTop+lengthBottom)*(heigth/2.0)이라고 적혀 있는데 해당 코드를 오라클에 입력하면 60.0이 나옵니다
    제가 아는 방식대로 손수 계산을 하면 52.5가 나오고요... (중학교부터 수학과 담 쌓고 지내던 문과여서 틀릴 수도 있습니다 틀렸으면 지적해 주세요)
    혹시 문제의 의도가 정확한 값을 구하는 게 아니라 실수를 만드는 건가요?
    
    만약 그렇다면 빈 칸에 (lengthTop+lengthBottom)*(double)(heigth/2)를 입력하면 오답일까요?
    저렇게 풀어도 60.0이 나오기는 했거든요...
    
    제 첫 답안이 후자여서 맞는 풀이인지 틀린 풀이인지 다양한 답안 중의 하나인지 궁금해서 글 남깁니다... 글 하나에 많은 질문이 있네요

---
+++ gradle/src/main/java/com/naver/cafe/thisisjava/wave1033513/SurfaceOfTrapezoidal.java
@@ -0,0 +1,11 @@
+package com.naver.cafe.thisisjava.wave1033513;
+
+public class SurfaceOfTrapezoidal {
+    public static void main(String[] args) {
+        int lengthTop = 5;
+        int lengthBottom = 10;
+        int height = 7;
+        double area = (                 );
+        System.out.println(area);
+    }
+}
