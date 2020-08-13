edeac1ff28b73474ce443866372e7109787e46c8
namo <nhkimnamo@gmail.com>
2020-08-13 13:34:15 +0900

    ref. https://cafe.naver.com/thisisjava/26246
    오답 적용: 52.0

--- gradle/src/main/java/com/naver/cafe/thisisjava/wave1033513/SurfaceOfTrapezoidal.java
+++ gradle/src/main/java/com/naver/cafe/thisisjava/wave1033513/SurfaceOfTrapezoidal.java
@@ -5,7 +5,7 @@ public class SurfaceOfTrapezoidal {
         int lengthTop = 5;
         int lengthBottom = 10;
         int height = 7;
-        double area = (lengthTop + lengthBottom) * (height / 2.0);
-        System.out.println(area);   // 52.5
+        double area = (lengthTop + lengthBottom) * height / 2;
+        System.out.println(area);   // 52.0
     }
 }
