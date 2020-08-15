fc1e4094d28c1b16536a1265966c9f296e8c0330
namhokim <nhkimnamo@gmail.com>
2020-08-15 20:42:11 +0900

    A. 만약 contains로 존재여부만 판단하는 것이 아닌, list1과 list2의 객체 정보를 모두 활용한다면 동일하게 Set으로 for를 실행해야 될 것 같은데요.    아래 첨부한 소스에서 list1의 p1 객체와, list2의 p2 객체 정보를 활용해야 한다면 더 나은 방법이 있을까요?

---
+++ gradle/src/main/java/com/naver/cafe/javachobostudy/kaidokid7/Ch6_6_7.java
@@ -0,0 +1,12 @@
+package com.naver.cafe.javachobostudy.kaidokid7;
+
+public class Ch6_6_7 {
+    public static void main(String[] args) {
+        Tv t;
+        Tv tv = new Tv();
+    }
+
+    static class Tv {
+
+    }
+}
