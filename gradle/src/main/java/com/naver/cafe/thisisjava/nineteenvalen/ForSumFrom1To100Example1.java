ddbf4d13a2aab87d4789cc739d7867ffa0670b12
namhokim <nhkimnamo@gmail.com>
2020-08-15 21:18:52 +0900

    굵게 칠한 부분이 1이 아니라 i여야 하는데 이렇게 넣고 돌리니까 5050이어야 하는 결과가 100이 나오더라고요
    제가 생각하기에는 저렇게 실수를 했으면 sum= sum+1이 되어서 위의 for(int i=1; i<=100; i++)와 관계없이 위에 선언된 sum=0에 단순히 1을 더한 1이 나오지 않을까 했는데 그렇지 않았습니다! 1이 있는 자리에 2를 넣으니 200이 되었고 대충 입력한 값 * 100이 된다는 걸 알았어요
    
    ​
    어떻게 이런 결과가 나오게 된 것인지 알 수 있을까요? 문제와 상관은 없겠지만 호기심이 들어 질문합니다
    
    ref. https://cafe.naver.com/thisisjava/26252

--- gradle/src/main/java/com/naver/cafe/thisisjava/nineteenvalen/ForSumFrom1To100Example1.java
+++ gradle/src/main/java/com/naver/cafe/thisisjava/nineteenvalen/ForSumFrom1To100Example1.java
@@ -4,7 +4,7 @@ public class ForSumFrom1To100Example1 {
     public static void main(String[] args) {
         int sum = 0;
         for (int i = 1; i <= 100; i++) {
-            sum += i;
+            sum += 1;
         }
         System.out.println("1~100 합: " + sum);
     }
