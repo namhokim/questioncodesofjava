54a0f486eefd3a11c95b825dc26d757e66800dee
namo <nhkimnamo@gmail.com>
2020-09-22 16:04:43 +0900

    93쪽 keyCodeExample 엔터의 키코드값은 13,10이라고 하셨는데 제 출력값은 10만나옵니다.
    94쪽 ContinueKeyCodeReadExample
    여기서도 13의 값이 나오지 않습니다.
    
    95쪽 QStopExample
    여기서도 13의 값이 나오지 않습니다.
    
    ---
    96쪽 ScannerExample
    abc를 입력하고 엔터를 눌렀을때
    입력된 문자열: "abc"  라고 출력되어야하는데
    출력이 안됩니다. 그래서 엔터를 한번 더 누르면 종료가 됩니다.
    
    ref. https://cafe.naver.com/thisisjava/26417

---
+++ gradle/src/main/java/com/naver/cafe/thisisjava/yeji930131/ContinueKeyCodeReadExample.java
@@ -0,0 +1,14 @@
+package com.naver.cafe.thisisjava.yeji930131;
+
+public class ContinueKeyCodeReadExample {
+    public static void main(String[] args) throws Exception {
+        // TODO Auto-generated method stub
+
+        int keyCode;
+
+        while(true) {      //while -> Hye
+            keyCode = System.in.read();
+            System.out.println("keyCode: " + keyCode);
+        }
+    }
+}
---
+++ gradle/src/main/java/com/naver/cafe/thisisjava/yeji930131/ScannerExample.java
@@ -0,0 +1,22 @@
+package com.naver.cafe.thisisjava.yeji930131;
+
+import java.util.Scanner;
+
+public class ScannerExample {
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+        Scanner scanner = new Scanner(System.in);
+
+        String inputData;
+
+        while (true) {
+            inputData = scanner.nextLine();
+            System.out.println("입력된 문자열: \"" + inputData + "\"");
+
+            if(inputData.equals("q")); {
+                break;
+            }
+        }
+        System.out.println("종료");
+}}
