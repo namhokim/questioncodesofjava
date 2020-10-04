037a0b6d03a65724997e39e3e20ff3cc9d290deb
namo <nhkimnamo@gmail.com>
2020-10-04 15:36:32 +0900

    추가 질문
    클릭해서 보셔요. 결과true나옵니다.
    디버깅하면 id가 다르게나옵니다.
    다른id두개가 자바라는 하나의 객체를 참조하고 있는건가요??
    
    ref. https://cafe.naver.com/thisisjava/26463

---
+++ gradle/src/main/java/com/naver/cafe/thisisjava/cis940320/StringTest.java
@@ -0,0 +1,7 @@
+package com.naver.cafe.thisisjava.cis940320;
+
+public class StringTest {
+    public static void main(String[] args) {
+        System.out.println(new String("자바").equals(new String("자바")));
+    }
+}
