2e7fe2cd0796132fc06d6b03ce89f38e22f51e5f
namo <nhkimnamo@gmail.com>
2020-12-20 12:55:53 +0900

    필드값(speed)을 main 메소드 내부에서 변경하려고 하면 오류가 뜨는데 왜 그런건가요?
    ref. https://cafe.naver.com/thisisjava/26712

---
+++ gradle/src/main/java/com/naver/cafe/thisisjava/ljyeon0713/car.java
@@ -0,0 +1,14 @@
+package com.naver.cafe.thisisjava.ljyeon0713;
+
+public class car {
+int speed;
+car() {
+speed=0;
+}
+void method() {
+speed = 10;
+}
+    public static void main(String[] args) {
+        speed=20;
+    }
+}
