8adb800c0f2a91a8934d0543de484eea8ab097dc
namo <nhkimnamo@gmail.com>
2020-12-20 12:57:01 +0900

    1. 클래스명 시작 문자 대문자로 변경
    ref. https://cafe.naver.com/thisisjava/26712

---
+++ gradle/src/main/java/com/naver/cafe/thisisjava/ljyeon0713/Car.java
@@ -0,0 +1,17 @@
+package com.naver.cafe.thisisjava.ljyeon0713;
+
+public class Car {
+    int speed;
+
+    Car() {
+        speed = 0;
+    }
+
+    void method() {
+        speed = 10;
+    }
+
+    public static void main(String[] args) {
+        speed = 20;
+    }
+}
