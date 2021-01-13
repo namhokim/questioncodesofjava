ddf56f2957327ecd8c2234d3b4239e636e6172fe
namo <nhkimnamo@gmail.com>
2021-01-13 12:49:09 +0900

    위와 같이 해봤는데 average에서 컴파일 오류가 발생합니다. 그래서 아래와 같이 클래스의 값을 확인해보니까...
    ref. https://cafe.naver.com/thisisjava/26843

--- gradle/src/main/java/com/naver/cafe/thisisjava/thevu/StreamMapIntegerAverage.java
+++ gradle/src/main/java/com/naver/cafe/thisisjava/thevu/StreamMapIntegerAverage.java
@@ -1,27 +1,14 @@
 package com.naver.cafe.thisisjava.thevu;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class StreamMapIntegerAverage {
     public static void main(String[] args) {
-        List<String> list2 = new ArrayList<>();
-        list2.add("1,2");
-        list2.add("3,4");
-
-        double output = list2.stream().flatMapToInt(data->{
-            String[] strArr = data.split(",");
-            int[] intArr = new int[strArr.length];
-
-            for(int i=0; i<strArr.length; i++){
-                intArr[i] = Integer.parseInt(strArr[i].trim());
-            }
-
-            return Arrays.stream(intArr);
-        })
-        .average()
-        .getAsDouble();
+        List<String> list2 = Arrays.asList("1,2,3", "4,5,6");
+        double output = list2.stream().flatMap(s-> Arrays.stream(s.split(",")))
+                .map(s->Integer.parseInt(s.trim()))
+                .average(); // cannot find symbol
 
         System.out.println(output);
     }
