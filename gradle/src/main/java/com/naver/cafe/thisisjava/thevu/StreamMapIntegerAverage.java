4a11967a58705c8af1cb795c1fc1da1682ab71dd
namo <nhkimnamo@gmail.com>
2021-01-13 12:50:44 +0900

    change to mapToInt
    ref. https://cafe.naver.com/thisisjava/26843

--- gradle/src/main/java/com/naver/cafe/thisisjava/thevu/StreamMapIntegerAverage.java
+++ gradle/src/main/java/com/naver/cafe/thisisjava/thevu/StreamMapIntegerAverage.java
@@ -2,12 +2,13 @@ package com.naver.cafe.thisisjava.thevu;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.OptionalDouble;
 
 public class StreamMapIntegerAverage {
     public static void main(String[] args) {
         List<String> list2 = Arrays.asList("1,2,3", "4,5,6");
-        double output = list2.stream().flatMap(s-> Arrays.stream(s.split(",")))
-                .map(s->Integer.parseInt(s.trim()))
+        OptionalDouble output = list2.stream().flatMap(s-> Arrays.stream(s.split(",")))
+                .mapToInt(s->Integer.parseInt(s.trim()))
                 .average(); // cannot find symbol
 
         System.out.println(output);
