b789cd2cc9e51ef495f6ea7c233b40854990cf27
namo <nhkimnamo@gmail.com>
2021-01-29 17:13:22 +0900

    강제타입변환에서 작은허용 범위타입 = (작은허용범위타입)큰 허용범위타입 이라고 하셨는데 double var = (double)intValue; 가 왜 성립되는지 이해가 가지않습니다.
    double타입이 int타입보다 더 크지 않나요..??
    
    ref. https://cafe.naver.com/thisisjava/26923

---
+++ gradle/src/main/java/com/naver/cafe/thisisjava/durms1125/ExplicitCast.java
@@ -0,0 +1,9 @@
+package com.naver.cafe.thisisjava.durms1125;
+
+public class ExplicitCast {
+    public static void main(String[] args) {
+        int intValue = 10;
+        double var = (double)intValue;
+        System.out.println(var);
+    }
+}
